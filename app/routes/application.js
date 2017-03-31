4618bf44313802e1b1f1423a9045aae1d4684241
jweg <jweg@informatik.uni-kiel.de>
2017-03-31 10:15:17 +0200

    addon-system integration

--- app/routes/application.js
+++ app/routes/application.js
@@ -7,9 +7,19 @@ const { Route } = Ember;
 
 export default Route.extend(ApplicationRouteMixin, {
 
-  actions: {
-    logout: function() {
+navbarService: Ember.inject.service('navbar'),
+
+init(){
+	this._super();
+	this.get('navbarService').navbarLabels.addObject('visualization');
+	this.get('navbarService').navbarLabels.addObject('tutorial');
+	},
+
+    logout() {
       this.get('session').invalidate();
     }
-  }
+    
+  
+
+  
 });
