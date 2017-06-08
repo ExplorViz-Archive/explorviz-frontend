8dd552ee2b6a3048a6f8d39d1956812acee075d3
Alexander Krause <akr@informatik.uni-kiel.de>
2017-06-08 18:14:38 +0200

    init: color picker

---
+++ app/components/configuration/color-selector.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+
+    // @Override
+  didRender() {
+    this._super(...arguments);
+  }
+
+});
---
+++ tests/integration/components/configuration/color-selector-test.js
@@ -0,0 +1,26 @@
+import { moduleForComponent, test } from 'ember-qunit';
+//import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('configuration/color-selector', 'Integration | Component | configuration/color selector', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  /*this.render(hbs`{{configuration/color-selector}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // Template block usage:
+  this.render(hbs`
+    {{#configuration/color-selector}}
+      template block text
+    {{/configuration/color-selector}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');*/
+  assert.equal(true, true);
+});
