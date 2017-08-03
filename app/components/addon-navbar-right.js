e278edbf74f9422e8feac42a47efbf4e0280193c
Ofi <stu105502@mail.uni-kiel.de>
2017-08-03 10:35:18 +0200

    einführung vom Senkrechten Strich zwischen normaler Navbar und Addon-navbar

--- app/components/addon-navbar-right.js
+++ app/components/addon-navbar-right.js
@@ -3,5 +3,6 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   configuration: Ember.inject.service("configuration"),
   tagName: "ul",
-  classNames:["nav navbar-nav navbar-right"]
+  classNames:["nav navbar-nav navbar-right"],
+  elementId: "addon-navbar-right"
 });
