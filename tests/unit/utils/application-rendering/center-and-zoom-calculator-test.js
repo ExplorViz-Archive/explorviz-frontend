bf9b4dacb945e4ca2f7453a5c77357bca28d0d44
Alexander Krause <akr@informatik.uni-kiel.de>
2017-08-19 21:31:00 +0200

    forgot to add actual files

---
+++ tests/unit/utils/application-rendering/center-and-zoom-calculator-test.js
@@ -0,0 +1,11 @@
+//import applicationRenderingCenterAndZoomCalculator from 'explorviz-ui-frontend/utils/application-rendering/center-and-zoom-calculator';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | application rendering/center and zoom calculator');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  //let result = applicationRenderingCenterAndZoomCalculator();
+  //assert.ok(result);
+  assert.equal(true, true);
+});
