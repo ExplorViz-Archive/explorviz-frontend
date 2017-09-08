9b5e8c07f1b38b425abee50394a7f18ff77635c8
Alexander Krause <akr@informatik.uni-kiel.de>
2017-09-08 17:47:33 +0200

    added: yuidoc-packages.js

---
+++ yuidoc-packages.js
@@ -0,0 +1,35 @@
+/**
+* Main module
+*
+* @module explorviz
+* @main explorviz
+*/
+
+
+
+/**
+* This module holds JS-files that are necessary for every Ember application. 
+*
+* @module ember
+* @main explorviz
+*/
+
+
+
+/**
+* Viz TODO 
+*
+* @module explorviz
+* @submodule visualization
+* @main visualization
+*/
+
+
+
+/**
+* Viz.Rend TODO 
+*
+* @module explorviz
+* @submodule visualization.rendering
+* @main visualization.rendering
+*/
