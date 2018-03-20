bec98563e01137d8fc4216b48d5b0c2a95f1c272
Christian Zirkelbach <czi@informatik.uni-kiel.de>
2018-03-20 11:01:50 +0100

    Updated yuidoc docs

--- docs/api.js
+++ docs/api.js
@@ -1,26 +1,138 @@
 YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
+        "Administration-Route",
+        "AggregatedClazzCommunication-Model",
+        "Alertify-Handler",
         "Application",
-        "Application-Rendering",
+        "Application-Controller",
+        "Application-Model",
+        "Application-Rendering-Component",
+        "Application-Route",
+        "Application-Serializer",
+        "ApplicationCommunication-Model",
         "Authenticator",
-        "BaseEntity",
-        "Clazz",
-        "Communication",
-        "CommunicationClazz",
-        "Component",
-        "Draw3DNodeEntity",
-        "DrawEdgeEntity",
-        "DrawNodeEntity",
-        "Landscape",
-        "Landscape-Rendering",
-        "Node",
-        "NodeGroup",
-        "Rendering-Core",
-        "System"
+        "Authorizer",
+        "Badroute-Route",
+        "BaseEntity-Model",
+        "Clazz-Model",
+        "ClazzCommunication-Model",
+        "Component-Model",
+        "Configuration-Controller",
+        "Configuration-Route",
+        "Configuration-Service",
+        "CumulatedClazzCommunication-Model",
+        "Data-Reload-Service",
+        "DatabaseQuery-Model",
+        "Draw3DNodeEntity-Model",
+        "DrawEdgeEntity-Model",
+        "DrawNodeEntity-Model",
+        "Index-Route",
+        "Landscape-Adapter",
+        "Landscape-Model",
+        "Landscape-Reload-Service",
+        "Landscape-Rendering-Component",
+        "Landscape-Repository-Service",
+        "Landscape-Serializer",
+        "Login-Form-Component",
+        "Login-Route",
+        "Navbar-Glyphicons",
+        "Navbar-Routes",
+        "Node-Model",
+        "NodeGroup-Model",
+        "Page-Setup-Service",
+        "Rendering-Core-Component",
+        "Router",
+        "RuntimeInformation-Model",
+        "System-Model",
+        "System-Serializer",
+        "ThreeJS-Performance-Mixin",
+        "Time-Shift-Component",
+        "Timeshift-Reload-Service",
+        "Timestamp-Adapter",
+        "Timestamp-Model",
+        "Timestamp-Repository-Service",
+        "Tutorial-Route",
+        "Visualization-Controller",
+        "Visualization-Route",
+        "user-Model"
     ],
-    "modules": [],
-    "allModules": [],
+    "modules": [
+        "explorviz",
+        "model",
+        "model.meta",
+        "model.util",
+        "network",
+        "page",
+        "security",
+        "settings",
+        "util",
+        "visualization",
+        "visualization.rendering",
+        "visualization.timeshift"
+    ],
+    "allModules": [
+        {
+            "displayName": "explorviz",
+            "name": "explorviz",
+            "description": "Main module"
+        },
+        {
+            "displayName": "model",
+            "name": "model",
+            "description": "Model TODO"
+        },
+        {
+            "displayName": "model.meta",
+            "name": "model.meta",
+            "description": "Model.Meta TODO"
+        },
+        {
+            "displayName": "model.util",
+            "name": "model.util",
+            "description": "Model.Util TODO"
+        },
+        {
+            "displayName": "network",
+            "name": "network",
+            "description": "Network TODO"
+        },
+        {
+            "displayName": "page",
+            "name": "page",
+            "description": "Page TODO"
+        },
+        {
+            "displayName": "security",
+            "name": "security",
+            "description": "Security TODO"
+        },
+        {
+            "displayName": "settings",
+            "name": "settings",
+            "description": "Settings TODO"
+        },
+        {
+            "displayName": "util",
+            "name": "util",
+            "description": "This method calculates a new color with a different brightness\ndegree as the passed color."
+        },
+        {
+            "displayName": "visualization",
+            "name": "visualization",
+            "description": "Viz TODO"
+        },
+        {
+            "displayName": "visualization.rendering",
+            "name": "visualization.rendering",
+            "description": "Viz.Rend TODO"
+        },
+        {
+            "displayName": "visualization.timeshift",
+            "name": "visualization.timeshift",
+            "description": "Viz.Time TODO"
+        }
+    ],
     "elements": []
 } };
 });
