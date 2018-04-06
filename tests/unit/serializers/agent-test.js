0d1f7338a3678b9e179aadbce8d5b8bec17bbf1e
Alexander Krause <alxander.krause@gmail.com>
2018-04-06 15:32:39 +0200

    fixed: failing travis build

--- tests/unit/serializers/agent-test.js
+++ tests/unit/serializers/agent-test.js
@@ -2,7 +2,7 @@ import { moduleForModel, test } from 'ember-qunit';
 
 moduleForModel('agent', 'Unit | Serializer | agent', {
   // Specify the other units that are required for this test.
-  needs: ['serializer:agent']
+  needs: ['serializer:agent', 'model:procezz']
 });
 
 // Replace this with your real tests.
