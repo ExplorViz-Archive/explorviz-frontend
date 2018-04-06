1abe00922e73949d520603999cb6944297ba3029
Josefine Wegert <jweg@informatik.uni-kiel.de>
2018-04-06 14:46:25 +0200

    this.store.unloadAll() stopped, because of error

--- app/routes/tutorial.js
+++ app/routes/tutorial.js
@@ -13,9 +13,10 @@ export default BaseRoute.extend(AuthenticatedRouteMixin, {
   // BUG
   // not 100% working unloading ember store, related to https://github.com/emberjs/data/issues/4938
   beforeModel() {
-    this.store.unloadAll();
-    this.store.unloadAll('databasequery');
-    this.store.unloadAll('timestamp');
+    //Error: Cannot update watchers for `systems` on `<explorviz-frontend@model:landscape::ember697:1>` after it has been destroyed."
+    // this.store.unloadAll();
+    // this.store.unloadAll('databasequery');
+    // this.store.unloadAll('timestamp');
   }
 
 });
