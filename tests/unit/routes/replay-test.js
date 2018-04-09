64ac25e8013551ff9af903fbe2e70548835fad1d
Josefine Wegert <jweg@informatik.uni-kiel.de>
2018-04-09 13:14:38 +0200

    test fixed

--- tests/unit/routes/replay-test.js
+++ tests/unit/routes/replay-test.js
@@ -2,7 +2,7 @@ import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('route:replay', 'Unit | Route | replay', {
   // Specify the other units that are required for this test.
-   needs: ['service:session', 'service:rendering-service']
+   needs: ['service:session', 'service:rendering-service', 'service:reload-handler','service:view-importer']
 });
 
 test('it exists', function(assert) {
