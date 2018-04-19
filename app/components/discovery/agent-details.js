2f52c7a7d7518b7f9fa750378474e09035d66b5a
Alexander Krause <alxander.krause@gmail.com>
2018-04-19 09:44:11 +0200

    use Ember.jquery package

--- app/components/discovery/agent-details.js
+++ app/components/discovery/agent-details.js
@@ -1,6 +1,7 @@
 import Component from '@ember/component';
 import { inject as service } from "@ember/service";
 import AlertifyHandler from 'explorviz-frontend/mixins/alertify-handler';
+import $ from "jquery";
 
 export default Component.extend(AlertifyHandler, {
 
