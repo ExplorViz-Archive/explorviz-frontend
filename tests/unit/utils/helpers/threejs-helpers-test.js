32fef7e5ee0403f00c94c35a670af6c23804fa9a
Alexander Krause <akr@informatik.uni-kiel.de>
2018-06-22 23:28:18 +0200

    fixed build error (due to new transpiling addon)

--- tests/unit/utils/helpers/threejs-helpers-test.js
+++ tests/unit/utils/helpers/threejs-helpers-test.js
@@ -1,6 +1,6 @@
 import { calculateColorBrightness } from 'explorviz-frontend/utils/helpers/threejs-helpers';
 import { module, test } from 'qunit';
-import THREE from "npm:three";
+import THREE from "three";
 
 module('Unit | Utility | helpers/threejs helpers');
 
