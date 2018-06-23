55157dec6dda5b30e4fcc16395ed1e9f425369c4
Alexander Krause <akr@informatik.uni-kiel.de>
2018-06-23 10:40:37 +0200

    changed debug message in resetRoute action to an error

--- app/routes/base-route.js
+++ app/routes/base-route.js
@@ -5,7 +5,10 @@ export default Route.extend({
   actions: {
 
     resetRoute() {
-      this.debug("not implemented");
+      const routeName = this.get('routeName');
+
+      throw new Error(`UnsupportedOperationException: Please implement the 
+      'resetRoute' action in the '${routeName}' route`);
     }
   }
 
