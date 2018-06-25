b02c2b4a44de058303b129939f8aa58c4f060e37
Alexander Krause <akr@informatik.uni-kiel.de>
2018-06-25 09:06:02 +0200

    Remove renderingService from application controller, since it is not needed at this level

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -15,7 +15,6 @@ import { computed } from "@ember/object";
 export default Controller.extend({
 
   session: service('session'),
-  renderingService: service('rendering-service'),
 
   username: computed(function(){
     return this.get('session').session.content.authenticated.username;
