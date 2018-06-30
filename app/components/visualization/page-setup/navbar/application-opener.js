05e96f7582e904c7540f6079ae5ea1c0d0eb083d
Alexander Krause <akr@informatik.uni-kiel.de>
2018-06-30 22:36:08 +0200

    added more components for navbar

---
+++ app/components/visualization/page-setup/navbar/application-opener.js
@@ -0,0 +1,26 @@
+import Component from '@ember/component';
+import { inject as service } from "@ember/service";
+
+export default Component.extend({
+
+  tagName: '',
+
+  store: service(),
+  renderingService: service(),
+
+  actions: {
+    openAllComponents() {
+      this.openAllComponents();
+    }
+  },
+
+  openAllComponents() {
+    const allClazzes = this.get('store').peekAll('clazz');
+
+    allClazzes.forEach(function (clazz) {
+      clazz.openParents();
+    });
+    this.get('renderingService').redrawScene();
+  }
+
+});
