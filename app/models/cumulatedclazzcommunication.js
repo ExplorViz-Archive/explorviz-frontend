5c1a6728da0d2f61b0a85a51c192b3fa44a2bebf
malte-hansen <stu126317@mail.uni-kiel.de>
2018-11-03 15:28:38 +0100

    Retrieve runtime/trace information for cumulatedclazzcommunication

--- app/models/cumulatedclazzcommunication.js
+++ app/models/cumulatedclazzcommunication.js
@@ -29,4 +29,38 @@ export default DrawEdgeEntity.extend({
     inverse: null
   }),
 
+  // returns boolean to tell whether the communication contains a trace with the given id
+  containsTrace(traceId) {
+    let runtimeInformations = this.getRuntimeInformations();
+    let found = false;
+
+    runtimeInformations.forEach( (runtimeInformation) => {
+      if (runtimeInformation.get('traceId') == traceId){
+        found = true;
+      }
+    });
+
+    return found;
+  },
+
+  // Returns a list of all underlying runtimeinformations
+  getRuntimeInformations(){
+    let containedRuntimeInformations = [];
+
+    this.get('aggregatedClazzCommunications').forEach((aggregatedClazzCommunication) => {
+
+      const clazzCommunications = aggregatedClazzCommunication.get('outgoingClazzCommunications');
+
+      clazzCommunications.forEach((clazzCommunication) => {
+          const runtimeInformations = clazzCommunication.get('runtimeInformations');
+          runtimeInformations.forEach((runtimeInformation) => {
+            containedRuntimeInformations.push(runtimeInformation);
+          });
+
+      });
+    });
+    
+    return containedRuntimeInformations;
+  }
+
 });
