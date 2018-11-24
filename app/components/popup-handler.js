5a2eb7da8d28c52d1d277f3e39154144d6b4f7e9
malte-hansen <stu126317@mail.uni-kiel.de>
2018-11-24 15:13:37 +0100

    Popup-Handler: Added draft for component and template

---
+++ app/components/popup-handler.js
@@ -0,0 +1,23 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+
+    communication: null,
+
+    /*
+    init(){
+        let communication = {
+            sourceClazz: 'SourceClazz', 
+            targetClazz: 'TargetClazz', 
+            isBidirectional: false,
+            requests: 30000,
+            traces: 3,
+            responseTime: 1,
+            duration: 30000};
+        
+        this.set('communication', communication);
+        this._super(...arguments);
+    }*/
+
+
+});
