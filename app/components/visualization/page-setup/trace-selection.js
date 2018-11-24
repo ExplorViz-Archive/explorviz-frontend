34534a75c15ffdfa1fea73f5a35fdd876eb77e3d
malte-hansen <stu126317@mail.uni-kiel.de>
2018-11-24 19:52:11 +0100

    Restore trace selection behaviour due to css-refactoring

--- app/components/visualization/page-setup/trace-selection.js
+++ app/components/visualization/page-setup/trace-selection.js
@@ -3,38 +3,45 @@ import { inject as service } from "@ember/service";
 
 export default Component.extend({
 
-  // No Ember generated container
-  tagName: '',
+    // No Ember generated container
+    tagName: '',
 
-  additionalData: service('additional-data'),
-  highlighter: service('visualization/application/highlighter'),
-  renderingService: service(),
+    additionalData: service('additional-data'),
+    highlighter: service('visualization/application/highlighter'),
+    renderingService: service(),
 
-  actions: {
-    traceSelected(traceId) {
-      let traces = this.get('additionalData.data');
+    init(){
+        this.get('additionalData').on('showWindow', this, this.onWindowChange);
+        this._super(...arguments);
+    },
 
-      // mark selected trace
-      traces.forEach( (trace) => {
-        if (trace.traceId == traceId){
-          trace.set('isSelected', true);
-        } else {
-          trace.set('isSelected', false);
+    actions: {
+        traceSelected(traceId) {
+            let traces = this.get('additionalData.data');
+
+            // mark selected trace
+            traces.forEach((trace) => {
+                if (trace.traceId == traceId) {
+                    trace.set('isSelected', true);
+                } else {
+                    trace.set('isSelected', false);
+                }
+            });
+
+            this.set('additionalData.data', traces);
+
+            this.get('highlighter').highlightTrace(traceId);
+            this.get('highlighter').applyHighlighting();
+            this.get('renderingService').redrawScene();
         }
-      });
+    },
 
-      this.set('additionalData.data', traces);
+    onWindowChange() {
+        if (!this.get('additionalData.showWindow') && this.get('highlighter.isTrace')) {
+            let highlighter = this.get('highlighter');
+            highlighter.unhighlightAll();
+            this.get('renderingService').redrawScene();
+        }
+    },
 
-      this.get('highlighter').highlightTrace(traceId);
-      this.get('highlighter').applyHighlighting();
-      this.get('renderingService').redrawScene();
-    }
-  },
-
-  willDestroyElement(){
-    // unhighlight trace
-    let highlighter = this.get('highlighter');
-    highlighter.unhighlightAll();
-    this.get('renderingService').redrawScene();
-  }
 });
