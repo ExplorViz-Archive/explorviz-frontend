3f271d186850e894fe907235b28500bc56007b69
stu126317 <stu126317@mail.uni-kiel.de>
2018-11-29 01:31:51 +0100

    Added files for landscape popups

---
+++ tests/integration/components/visualization/rendering/landscape-popups/node-popup-test.js
@@ -0,0 +1,26 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | node-popup', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
+
+    await render(hbs`{{node-popup}}`);
+
+    assert.equal(this.element.textContent.trim(), '');
+
+    // Template block usage:
+    await render(hbs`
+      {{#node-popup}}
+        template block text
+      {{/node-popup}}
+    `);
+
+    assert.equal(this.element.textContent.trim(), 'template block text');
+  });
+});
