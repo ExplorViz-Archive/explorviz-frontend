7e72dedc69fd905f58630c8cbae7e82a1ee548b2
malte-hansen <stu126317@mail.uni-kiel.de>
2018-12-08 15:40:05 +0100

    Prevent popup positioning outside of canvas

--- app/components/visualization/rendering/popups/popup-coordinator.js
+++ app/components/visualization/rendering/popups/popup-coordinator.js
@@ -1,35 +1,56 @@
 import Component from '@ember/component';
 import $ from 'jquery';
-import {inject as service} from '@ember/service';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
 
-    additionalData: service(),
+  additionalData: service(),
 
-    didRender(){
-        this._super(...arguments);
+  didRender() {
+    this._super(...arguments);
 
-        if (this.get('additionalData.popupContent')){
-            this.setPopupPosition();
-        }
-    },
-
-    setPopupPosition(){
-        let popupData = this.get('additionalData.popupContent');
-
-        const popoverDiv = $('.popover');
-        const containerDiv = $('.main-content');
-
-        const containerTopOffset = containerDiv.offset().top;
-        const containerLeftOffset = containerDiv.offset().left;
-    
-        const popupTopOffset = popoverDiv.height();
-        const popupLeftOffset = popoverDiv.width() / 2;
-
-        const popupTopPosition = containerTopOffset + popupData.mouseY - popupTopOffset;
-        const popupLeftPosition = containerLeftOffset + popupData.mouseX - popupLeftOffset;
-
-        $('.popover').css('top', popupTopPosition + 'px');
-        $('.popover').css('left', popupLeftPosition + 'px');
+    if (this.get('additionalData.popupContent')) {
+      this.setPopupPosition();
     }
+  },
+
+  setPopupPosition() {
+    let popupData = this.get('additionalData.popupContent');
+
+    const popoverDiv = $('.popover');
+
+    // Sorrounding div for position calculations
+    const containerDiv = $('.main-content');
+
+    const containerTopOffset = containerDiv.offset().top;
+    const containerLeftOffset = containerDiv.offset().left;
+
+    const popupTopOffset = popoverDiv.height() + 10;
+    const popupLeftOffset = popoverDiv.width() / 2;
+
+    let popupTopPosition = containerTopOffset + popupData.mouseY - popupTopOffset;
+    let popupLeftPosition = containerLeftOffset + popupData.mouseX - popupLeftOffset;
+
+    // Prevent popup positioning on top of rendering canvas => 
+    // position under mouse cursor
+    if (popupTopPosition < containerTopOffset) {
+      popupTopPosition = containerTopOffset + popupData.mouseY + 35;
+    }
+
+    // Prevent popup positioning right(outside) of rendering canvas => 
+    // position at right edge of canvas
+    if (popupLeftPosition + popoverDiv.width() > containerLeftOffset + containerDiv.width()) {
+      popupLeftPosition = containerLeftOffset + containerDiv.width() - popoverDiv.width() - 5;
+    }
+
+    // Prevent popup positioning left(outside) of rendering canvas => 
+    // position at left edge of canvas
+    if (popupLeftPosition < containerLeftOffset) {
+      popupLeftPosition = containerLeftOffset;
+    }
+
+    // Set popup position
+    $('.popover').css('top', popupTopPosition + 'px');
+    $('.popover').css('left', popupLeftPosition + 'px');
+  }
 })
