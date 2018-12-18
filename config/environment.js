0a6e8fde27f8cd740785e791685026d7b8929e9d
Alexander Krause <akr@informatik.uni-kiel.de>
2018-12-18 18:15:58 +0100

    messages on build startup, depending on the used environment

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,7 @@
 /* jshint node: true */
 
+var colors = require('colors');
+
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'explorviz-frontend',
@@ -24,16 +26,40 @@ module.exports = function(environment) {
   };
 
   if (environment === 'development') {
-    ENV.APP.API_ROOT = 'http://localhost:8080';
+
+    var API_ROOT = 'http://localhost:8080';
+    ENV.APP.API_ROOT = API_ROOT;
+
+    console.log(`Development mode: Using ${API_ROOT} as API_ROOT`.yellow);
   }
 
   if (environment === 'production') {
-    ENV.rootURL = '/explorviz-frontend';
-    ENV.APP.API_ROOT = '/explorviz-backend';
+
+    var rootURL = '/explorviz-frontend';
+    var API_ROOT = '/explorviz-backend';
+
+    if(process.env.ROOT_URL) {
+      rootURL = process.env.ROOT_URL;
+      console.log(`Production mode: Using ${rootURL} as rootURL`.yellow);
+    } else {
+      console.log(`ATTENTION: Production mode, but using default value ${rootURL} as rootURL`.yellow);
+    }
+
+    if(process.env.API_ROOT) {
+      API_ROOT = process.env.API_ROOT;
+      console.log(`Production mode: Using ${API_ROOT} as API_ROOT`.yellow);
+    } else {
+      console.log(`ATTENTION: Production mode, but using default value ${rootURL} as rootURL`.yellow);
+    }
+
+    ENV.rootURL = rootURL;
+    ENV.APP.API_ROOT = API_ROOT;
   }
 
   if (environment === 'mocked') {
+    var API_ROOT = 'http://localhost:4200/api';
     ENV.APP.API_ROOT = 'http://localhost:4200/api';
+    console.log(`Mocked API mode: Using ${API_ROOT} as API_ROOT`.yellow);
   }
 
   if (environment === 'test') {
@@ -47,9 +73,7 @@ module.exports = function(environment) {
     ENV.APP.rootElement = '#ember-testing';
   }
 
-
   // User specific environment, e.g. for usage in a virtual machine
-
 
   if (environment === 'akr') {
     ENV.APP.API_ROOT = 'http://192.168.91.132:8080';
