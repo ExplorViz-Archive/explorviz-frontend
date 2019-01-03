370fd99c1c27e0382edff92a9bcdae7e020a0591
Alexander Krause <akr@informatik.uni-kiel.de>
2019-01-03 12:40:53 +0100

    debugging travis build error

--- .travis/docker_push_dev.sh
+++ .travis/docker_push_dev.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+echo "Building Docker image"
+docker build -t explorviz/explorviz-frontend:dev .
 echo "$DOCKER_PW" | docker login -u "$DOCKER_LOGIN" --password-stdin
 docker push explorviz/explorviz-frontend:dev
