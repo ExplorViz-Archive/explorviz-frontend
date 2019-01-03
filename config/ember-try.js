f28561c0685b6d88e5db43743b903935e8747ac1
Alexander Krause <akr@informatik.uni-kiel.de>
2019-01-03 09:22:00 +0100

    added new ember version to ember try config

--- config/ember-try.js
+++ config/ember-try.js
@@ -14,34 +14,42 @@ module.exports = function() {
           npm: {
             devDependencies: {
               'ember-source': '~2.12.0',
-            },
-          },
+            }
+          }
         },
         {
           name: 'ember-lts-2.16',
           npm: {
             devDependencies: {
               'ember-source': '~2.16.0',
-            },
-          },
+            }
+          }
         },
         {
           name: 'ember-lts-2.18',
           npm: {
             devDependencies: {
               'ember-source': '~2.18.0',
-            },
-          },
+            }
+          }
         },
         {
-          name: 'ember-3.2.0',
+          name: 'ember-lts-3.4.7',
           npm: {
             devDependencies: {
-              'ember-source': '~3.2.0',
-            },
-          },
+              'ember-source': '~3.4.7',
+            }
+          }
+        },
+        {
+          name: 'ember-3.5.0 current DEV version',
+          npm: {
+            devDependencies: {
+              'ember-source': '~3.5.0',
+            }
+          }
         }
-      ],
+      ]
     };
   });
 };
