c72d94aa7fa1d0bcc6cab649e1109b6e3a7894c0
malte-hansen <stu126317@mail.uni-kiel.de>
2019-01-26 13:48:31 +0100

    Check if data for traces/dbQueries exists, inform user if not

--- app/components/visualization/page-setup/navbar/sql-opener.js
+++ app/components/visualization/page-setup/navbar/sql-opener.js
@@ -1,15 +1,21 @@
 import Component from '@ember/component';
 import { inject as service } from "@ember/service";
+import AlertifyHandler from 'explorviz-frontend/mixins/alertify-handler';
 
-export default Component.extend({
+export default Component.extend(AlertifyHandler, {
 
   // No Ember generated container
   tagName: '',
-
+  
   additionalData: service(),
+  landscapeRepo: service('repos/landscape-repository'),
 
   actions: {
     showSql() {
+      if (this.get("landscapeRepo.latestApplication.databaseQueries.length") === 0){
+        this.showAlertifyMessage("No SQL Queries found!");
+        return;
+      }
       this.get('additionalData').addComponent("visualization/page-setup/sidebar/sql-viewer");
       this.get('additionalData').openAdditionalData();
     },
--- app/components/visualization/page-setup/navbar/trace-overview.js
+++ app/components/visualization/page-setup/navbar/trace-overview.js
@@ -1,14 +1,21 @@
 import Component from '@ember/component';
 import { inject as service } from "@ember/service";
+import AlertifyHandler from 'explorviz-frontend/mixins/alertify-handler';
 
-export default Component.extend({
+export default Component.extend(AlertifyHandler, {
 
   // No Ember generated container
   tagName: '',
-  additionalData: service(),
 
+  additionalData: service(),
+  landscapeRepo: service('repos/landscape-repository'),
+  
   actions: {
     showTraces() {
+      if (this.get("landscapeRepo.latestApplication.traces.length") === 0){
+        this.showAlertifyMessage("No Traces found!");
+        return;
+      }
       this.get('additionalData').addComponent("visualization/page-setup/sidebar/trace-selection");
       this.get('additionalData').openAdditionalData();
     }
