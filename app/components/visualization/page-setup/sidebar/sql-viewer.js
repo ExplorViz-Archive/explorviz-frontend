6ae8000cf7b2a6c7d18cce8fe9682933aa8770e5
malte-hansen <stu126317@mail.uni-kiel.de>
2019-01-27 17:52:24 +0100

    Added detailed view for database queries

--- app/components/visualization/page-setup/sidebar/sql-viewer.js
+++ app/components/visualization/page-setup/sidebar/sql-viewer.js
@@ -6,8 +6,24 @@ export default Component.extend({
   tagName: '',
   landscapeRepo: service("repos/landscape-repository"),
   additionalData: service('additional-data'),
+  store: service(),
 
   actions: {
+    queryClicked(query){
+      // allow deselection of query
+      if (query.get('isSelected')){
+        query.set('isSelected', false);
+        return;
+      }
+      // deselect potentially selected query
+      let queries = this.get('store').peekAll('databasequery');
+      queries.forEach((query) => {
+        query.set('isSelected', false);
+      });
+      // mark new query as selected
+      query.set('isSelected', true);
+    },
+
     close() {
       this.get('additionalData').removeComponent("visualization/page-setup/sidebar/sql-viewer");
     },
