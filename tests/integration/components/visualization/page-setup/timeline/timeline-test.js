b965253316de9e0053d3be1f1693612b7d90fb12
Christian Zirkelbach <czi@informatik.uni-kiel.de>
2019-02-04 15:51:26 +0100

    more timestamp to date tests

--- tests/integration/components/visualization/page-setup/timeline/timeline-test.js
+++ tests/integration/components/visualization/page-setup/timeline/timeline-test.js
@@ -6,7 +6,7 @@ import hbs from 'htmlbars-inline-precompile';
 module('Integration | Component | visualization/page-setup/timeline/timeline', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
+  test('it renders', async function (assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
@@ -17,10 +17,9 @@ module('Integration | Component | visualization/page-setup/timeline/timeline', f
     // Template block usage:
     await render(hbs`
       {{#visualization/page-setup/timeline/timeline}}
-        template block text
       {{/visualization/page-setup/timeline/timeline}}
     `);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.equal(this.element.textContent.trim(), '');
   });
 });
--- tests/unit/helpers/timestamp-to-date-test.js
+++ tests/unit/helpers/timestamp-to-date-test.js
@@ -2,9 +2,25 @@
 import { timestampToDate } from 'explorviz-frontend/helpers/timestamp-to-date';
 import { module, test } from 'qunit';
 
-module('Unit | Helper | timestampt to date');
+module('Unit | Helper | timestamp to date');
 
 test('timestamp to time', function (assert) {
+  let assertedResult = "15:32:18";
   let result = timestampToDate([1549204338000, "time"]);
-  assert.ok(result);
+
+  assert.equal(result, assertedResult);
+});
+
+test('timestamp to toLocalString', function (assert) {
+  let assertedResult= "2/3/2019, 3:32:18 PM";
+  let result = timestampToDate([1549204338000, "localString"]);
+  
+  assert.equal(result, assertedResult);
+});
+
+test('timestamp to String', function (assert) {
+  let assertedResult= "Sun Feb 03 2019 15:32:18 GMT+0100 (Central European Standard Time)";
+  let result = timestampToDate([1549204338000, ""]);
+  
+  assert.equal(result, assertedResult);
 });
