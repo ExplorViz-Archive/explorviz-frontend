8bd61d17434c59bebc7f27befefc850070767d01
Christian Zirkelbach <czi@informatik.uni-kiel.de>
2019-02-11 14:05:48 +0100

    updated TravisCI towards yuidoc automation

--- .travis/yuidoc_push.sh
+++ .travis/yuidoc_push.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 
-if [ "$TRAVIS_REPO_SLUG" == "ExplorViz/explorviz-frontend" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+echo -e "Publishing yuidoc...\n"
 
-  echo -e "Publishing yuidoc...\n"
+yuidoc .
+cp -R docs $HOME/docs
 
-  yuidoc .
-  cp -R docs $HOME/docs
+cd $HOME
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+git clone --quiet --branch=gh-pages https://$PersonalAccessToken@github.com/ExplorViz/explorviz-frontend gh-pages > /dev/null
 
-  cd $HOME
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis CI"
-  git clone --quiet --branch=gh-pages https://$PersonalAccessToken@github.com/ExplorViz/explorviz-frontend gh-pages > /dev/null
+cd gh-pages
+git rm -rf *
+cp -Rf $HOME/docs/* .
+git add -f .
+git commit -m "Latest Yuidoc on successful Travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+git push -fq origin gh-pages > /dev/null
 
-  cd gh-pages
-  git rm -rf *
-  cp -Rf $HOME/docs/* .
-  git add -f .
-  git commit -m "Latest Yuidoc on successful Travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
-  git push -fq origin gh-pages > /dev/null
-
-  echo -e "Published Yuidoc to gh-pages.\n"
+echo -e "Published Yuidoc to gh-pages.\n"
 
 fi
