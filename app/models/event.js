5b5ac965b8ba23e980c0321409d925e4c44414cf
Christian Zirkelbach <czi@informatik.uni-kiel.de>
2019-02-14 15:13:17 +0100

    added model for events

---
+++ app/models/event.js
@@ -0,0 +1,21 @@
+import BaseEntity from './baseentity';
+import DS from 'ember-data';
+
+const { attr } = DS;
+
+/**
+ * Ember model for an event occurring in the landscape.
+ *
+ * @class Event-Model
+ * @extends BaseEntity-Model
+ *
+ * @module explorviz
+ * @submodule model.meta
+ */
+export default BaseEntity.extend({
+
+    timestamp: attr('number'),
+    eventType: attr('string'),
+    eventMessage: attr('string'),
+
+});
