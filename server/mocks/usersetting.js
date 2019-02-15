bdce865b82185a84d1a96b70926c02108815bf85
dan0x09 <danielkoenig95@web.de>
2019-02-15 16:41:56 +0100

    Mock user-settings descriptors

--- server/mocks/usersetting.js
+++ server/mocks/usersetting.js
@@ -4,7 +4,7 @@
 module.exports = function(app) {
   const express = require('express');
   let tokensRouter = express.Router();
-
+  
   const defaultSettings = {  
     "data":{  
       "type":"usersetting",
@@ -27,8 +27,91 @@ module.exports = function(app) {
     }
   };
 
+  const descriptors = {
+    "keepHighlightingOnOpenOrClose": {
+      "data":{
+        "type":"booleansettingsdescriptor",
+        "id":"keepHighlightingOnOpenOrClose",
+        "attributes":{
+          "description":"Transparency effect for selection (left click) in application visualization",
+          "defaultValue":true,
+          "name":"Keep Highlighting On Open Or Close"
+        }
+      }
+    },
+    "enableHoverEffects": {
+      "data":{
+        "type":"booleansettingsdescriptor",
+        "id":"enableHoverEffects",
+        "attributes":{
+          "description":"Hover effect (flashing entities) for mouse cursor",
+          "defaultValue":true,
+          "name":"Enable Hover Effects"
+        }
+      }
+    },
+    "showFpsCounter": {
+      "data":{
+        "type":"booleansettingsdescriptor",
+        "id":"showFpsCounter",
+        "attributes":{
+          "description":"'Frames Per Second' metrics in visualizations",
+          "defaultValue":false,
+          "name":"Show FPS Counter"
+        }
+      }
+    },
+    "appVizTransparency": {
+      "data":{
+        "type":"booleansettingsdescriptor",
+        "id":"appVizTransparency",
+        "attributes":{
+          "description":"Transparency effect for selection (left click) in application visualization",
+          "defaultValue":true,
+          "name":"App Viz Transparency"
+        }
+      }
+    },
+    "appVizCommArrowSize": {
+      "data":{
+        "type":"numericsettingsdescriptor",
+        "id":"appVizCommArrowSize",
+        "attributes":{
+          "description":"Arrow Size for selected communications in application visualization",
+          "defaultValue":1.0,
+          "min":-9.223372036854776E18,
+          "max":9.223372036854776E18,
+          "inRange":false,
+          "name":"AppViz Arrow Size"
+        }
+      }
+    },
+    "appVizTransparencyIntensity": {
+      "data":{
+        "type":"numericsettingsdescriptor",
+        "id":"appVizTransparencyIntensity",
+        "attributes":{
+          "description":"Transparency effect intensity ('App Viz Transparency' must be enabled)",
+          "defaultValue":0.1,
+          "min":0.1,
+          "max":0.5,
+          "inRange":false,
+          "name":"AppViz Transparency Intensity"
+        }
+      }
+    }
+  };
+
   tokensRouter.get('/', function(req, res) {
     res.send(defaultSettings);
+  });
+
+  tokensRouter.get('/:id', function(req, res) {
+    if(descriptors[req.params.id]) {
+      res.send(descriptors[req.params.id]);
+    } else {
+      res.send(400, {"errors": [ { "status": "400", "title": "Error", "detail": "Descriptor does not exists" } ]});
+    }
   });
 
   // The POST and PUT call will not contain a request body
