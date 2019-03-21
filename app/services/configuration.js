7c7e43f13d5262511f629a21e251d5f368d34f72
Christian Zirkelbach <czi@informatik.uni-kiel.de>
2019-03-21 13:39:44 +0100

    added visually impaired colors

--- app/services/configuration.js
+++ app/services/configuration.js
@@ -61,6 +61,7 @@ export default Service.extend({
    * Sets the default visualization colors
    */
   initDefaultColors() {
+
     this.set('landscapeColorsDefault', {
       system: "rgb(199, 199, 199)",
       nodegroup: "rgb(1, 155, 32)",
@@ -79,6 +80,35 @@ export default Service.extend({
       componentOdd: "rgb(0, 187, 65)",
       componentEven: "rgb(22, 158, 43)",
       clazz: "rgb(62, 20, 160)",
+      highlightedEntity: "rgb(255, 0, 0)",
+      communication: "rgb(244, 145, 0)",
+      communicationArrow: "rgb(0, 0, 0)"
+    });
+  },
+
+  /**
+   * Sets the current colors towards more visually impaired ones
+   */
+  applyVisuallyImpairedColors() {
+
+    this.set('landscapeColors', {
+      system: "rgb(199, 199, 199)",
+      nodegroup: "rgb(1, 90, 110)",
+      node: "rgb(0, 150, 190)",
+      application: "rgb(95, 95, 95)",
+      communication: "rgb(244, 145, 0)",
+      textsystem: "rgb(0, 0, 0)",
+      textnode: "rgb(255, 255, 255)",
+      textapp: "rgb(255, 255, 255)",
+      collapseSymbol: "rgb(0, 0, 0)",
+      textchanged: false
+    });
+
+    this.set('applicationColorsDefault', {
+      foundation: "rgb(199, 199, 199)",
+      componentOdd: "rgb(1, 90, 110)",
+      componentEven: "rgb(0, 150, 190)",
+      clazz: "rgb(95, 95, 95)",
       highlightedEntity: "rgb(255, 0, 0)",
       communication: "rgb(244, 145, 0)",
       communicationArrow: "rgb(0, 0, 0)"
