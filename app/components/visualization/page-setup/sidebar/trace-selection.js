9b0c904c3b79de5910ae7075cc8f83377dfd0794
Alexander Krause <akr@informatik.uni-kiel.de>
2019-04-09 13:06:44 +0200

    remove unused variable

--- app/components/visualization/page-setup/sidebar/trace-selection.js
+++ app/components/visualization/page-setup/sidebar/trace-selection.js
@@ -2,8 +2,6 @@ import Component from '@ember/component';
 import { inject as service } from "@ember/service";
 import { computed } from '@ember/object';
 
-import THREE from "three";
-
 export default Component.extend({
 
   // No Ember generated container
