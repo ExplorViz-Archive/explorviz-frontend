abaa2f90b117cdf0a76d7b0ecf0227739932fa88
dan0x09 <stu126132@mail.uni-kiel.de>
2019-05-30 01:26:56 +0200

    Create flagsetting and rangesetting models
    The former booleansettingsdescriptor and numericsettingsdescriptor are
    now named flagsetting and ragesetting, respectively. Furthermore, the
    setting name is now called displayName.

--- app/models/flagsetting.ts
+++ app/models/flagsetting.ts
@@ -2,17 +2,17 @@ import DS from 'ember-data';
 
 const { attr } = DS;
 
-export default class BooleanSettingsDescriptor extends DS.Model {
+export default class FlagSetting extends DS.Model {
 
   @attr('string') description!: string;
   
-  @attr('string') name!: string;
+  @attr('string') displayName!: string;
 
   @attr('boolean') defaultValue!: boolean;
 }
 
 declare module 'ember-data/types/registries/model' {
 	export default interface ModelRegistry {
-	  'booleansettingsdescriptor': BooleanSettingsDescriptor;
+	  'flagsetting': FlagSetting;
 	}
 }
