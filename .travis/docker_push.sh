56c391eb48a2011b330addf44ac810a049702669
Alexander Krause <akr@informatik.uni-kiel.de>
2019-06-14 19:39:42 +0200

    Wrong docker tag remote labeling

--- .travis/docker_push.sh
+++ .travis/docker_push.sh
@@ -8,15 +8,15 @@ echo "$DOCKER_PW" | docker login -u "$DOCKER_LOGIN" --password-stdin
 if [[ "$TRAVIS_BRANCH" == 'dev-1' ]]; then
   docker build -t explorviz-frontend .
   
-  docker tag explorviz-frontend explorviz-frontend:dev-$TRAVIS_COMMIT
-  docker tag explorviz-frontend explorviz-frontend:dev
+  docker tag explorviz-frontend explorviz/explorviz-frontend:dev-$TRAVIS_COMMIT
+  docker tag explorviz-frontend explorviz/explorviz-frontend:dev
   
   docker push explorviz/explorviz-frontend
 elif [[ "$TRAVIS_BRANCH" == 'master' ]]; then
   docker build -t explorviz-frontend .
   
-  docker tag explorviz-frontend explorviz-frontend:latest-$TRAVIS_COMMIT
-  docker tag explorviz-frontend explorviz-frontend:latest
+  docker tag explorviz-frontend explorviz/explorviz-frontend:latest-$TRAVIS_COMMIT
+  docker tag explorviz-frontend explorviz/explorviz-frontend:latest
   
   docker push explorviz/explorviz-frontend
 else
