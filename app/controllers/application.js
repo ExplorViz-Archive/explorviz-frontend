677f13ad65cb8b2ee8722f92c887e3c2d626e4ca
dan0x09 <stu126132@mail.uni-kiel.de>
2019-09-10 22:50:46 +0200

    Refactor applicaton route/controller
    In the current-user component, the load function now peeks for the
    user record, since the record should already exist.
    The methods for loading settings, the current user and the user
    preferences were moved to the application controller.
    The methods are partially async now. That way, it is no longer necessary
    to use promises as return values.
    With the loadUserAndSettings task, we can make sure that the navbar
    will only be displayed when both the user was authenticated and all
    settings and preferences were loaded. Before, the navbar was diaplyed
    next to the login form for a moment before moving to the visualization
    route.
    This commit also fixes the bug that was causing the user settings not to
    be displayed when entering a direct url, as the component was initialized
    before the settings in the application route were loaded.

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,5 +1,7 @@
 import Controller from '@ember/controller';
 import { inject as service } from "@ember/service";
+import { task } from 'ember-concurrency';
+import { isEmpty } from '@ember/utils';
 
 /**
 * TODO
@@ -12,6 +14,42 @@ import { inject as service } from "@ember/service";
 */
 export default Controller.extend({
   
-  session: service('session')
+  session: service(),
+  currentUser: service(),
+  userSettings: service(),
+
+  loadUserAndSettings: task(function * () {
+    this._loadCurrentUser();
+    yield this._loadCurrentUserPreferences();
+    yield this._loadSettingsAndTypes();
+  }),
+
+  _loadCurrentUser() {
+    return this.get('currentUser').load().catch(() => this.get('session').invalidate({ message: 'User could not be loaded' }));
+  },
+
+  async _loadCurrentUserPreferences() {
+    let userId = this.get('session').get('session.content.authenticated.rawUserData.data.id');
+    if (!isEmpty(userId)) {
+      await this.store.query('userpreference', { userId }).catch(() => this.get('session').invalidate({message: 'User preferences could not be loaded'}));
+    } else {
+      this.get('session').invalidate({ message: 'Session invalid' });
+    }
+  },
+
+  async _loadSettingsAndTypes() {
+    try {
+      // request all settings and load into store
+      let settings = await this.get('store').query('settingsinfo', 1);
+      // get all setting types and save them for future access
+      let types = new Set();
+      for (let i = 0; i < settings.get('length'); i++) {
+        types.add(settings.objectAt(i).constructor.modelName);
+      }
+      this.get('userSettings').set('types', types);
+    } catch(e) {
+      this.get('session').invalidate({ message: 'Settings could not be loaded' });
+    }
+  }
 
 });
