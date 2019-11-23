fa0f324d063d3b0bc463154d2511aaa63b896e07
dan0x09 <stu126132@mail.uni-kiel.de>
2019-11-23 02:30:53 +0100

    Remove deprecated route constraint addon
    ember-route-constraints is deprecated and therefore we do the
    permission check inside the beforeModel hook of a route now.

--- app/routes/configuration/usermanagement.ts
+++ app/routes/configuration/usermanagement.ts
@@ -1,11 +1,16 @@
 import BaseRoute from 'explorviz-frontend/routes/base-route';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
-import Constrainable from 'ember-route-constraints/mixins/constrainable';
-import {inject as service} from '@ember/service';
+import { inject as service } from '@ember/service';
 import CurrentUser from 'explorviz-frontend/services/current-user';
+import RouterService from '@ember/routing/router-service';
 
-export default class ConfigurationUserManagementRoute extends BaseRoute.extend(AuthenticatedRouteMixin, Constrainable) {
-  //used for route constraints
-  @service('current-user')
-  currentUser!: CurrentUser;
+export default class ConfigurationUserManagementRoute extends BaseRoute.extend(AuthenticatedRouteMixin) {
+  @service('current-user') currentUser!: CurrentUser;
+  @service('router') router!: RouterService;
+
+  beforeModel() {
+    // restrict route and sub-routes to admin only
+    if(!this.currentUser.user.hasRole('admin'))
+      this.router.transitionTo('configuration.settings');
+  }
 }
