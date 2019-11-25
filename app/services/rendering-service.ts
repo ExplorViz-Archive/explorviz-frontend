528fb553b693420ec9546d0cd36fb4c844fc944f
dan0x09 <danielkoenig95@web.de>
2019-11-25 18:08:00 +0100

    Extend rendering-service to govern rendering contexts and pipelines
    The rendering-service now has two new Maps to maintain both
    the rendering contexts (scene, renderer, camera) and rendering
    pipelines, which are function arrays. Futhermore, methods were
    defined for easily access data s.a. the scene. To easily apply
    the the rendering pipeline, a method called 'render' was added,
    which calls the functions of a pipeline sequantially.

--- app/services/rendering-service.ts
+++ app/services/rendering-service.ts
@@ -3,11 +3,23 @@ import Evented from '@ember/object/evented';
 import Clazz from 'explorviz-frontend/models/clazz';
 import DrawableClazzCommunication from 'explorviz-frontend/models/drawableclazzcommunication';
 import ClazzCommunication from 'explorviz-frontend/models/clazzcommunication';
+import THREE from "three";
+import Landscape from 'explorviz-frontend/models/landscape';
+import Application from 'explorviz-frontend/models/application';
+
+export interface RenderingContext {
+  scene: THREE.Scene,
+  camera: THREE.Camera,
+  renderer: THREE.WebGLRenderer
+}
 
 export default class RenderingService extends Service.extend(Evented) {
 
   showTimeline: boolean = true;
   showVersionbar: boolean = true;
+
+  renderingContexts: Map<string, RenderingContext> = new Map();
+  customRenderingPipelines: Map<string, Function[]> = new Map();
 
   reSetupScene() {
     // Redraws and repositions scene to default
@@ -33,6 +45,78 @@ export default class RenderingService extends Service.extend(Evented) {
 
   toggleTimeline() {
     this.toggleProperty('showTimeline');
+  }
+
+  addRendering(id: string, context: RenderingContext, pipeline: Function[]) : boolean {
+    if(this.renderingContexts.get(id) !== undefined || this.customRenderingPipelines.get(id) !== undefined) {
+      // there already exists a rendering with passed id
+      return false;
+    }
+    this.renderingContexts.set(id, context);
+    this.customRenderingPipelines.set(id, pipeline);
+    return true;
+  }
+
+  removeRendering(id: string) {
+    this.renderingContexts.delete(id);
+    this.customRenderingPipelines.delete(id);
+  }
+
+  overwritePipeline(id: string, pipeline: Function[]) : boolean {
+    const oldPipeline = this.getPipelineById(id);
+    if(oldPipeline === undefined || pipeline.length <= 0)
+      return false;
+  
+    this.customRenderingPipelines.set(id, pipeline);
+  
+    return true;
+  }
+
+  getSceneById(id: string) {
+    const renderingContext = this.renderingContexts.get(id);
+    if(renderingContext === undefined)
+      return undefined;
+
+    return renderingContext.scene;
+  }
+
+  getCameraById(id: string) {
+    const renderingContext = this.renderingContexts.get(id);
+    if(renderingContext === undefined)
+      return undefined;
+
+    return renderingContext.camera;
+  }
+
+  getRendererById(id: string) {
+    const renderingContext = this.renderingContexts.get(id);
+    if(renderingContext === undefined)
+      return undefined;
+
+    return renderingContext.renderer;
+  }
+
+  getCanvasById(id: string) {
+    const renderingContext = this.renderingContexts.get(id);
+    if(renderingContext === undefined)
+      return undefined;
+
+    return renderingContext.renderer.domElement;
+  }
+
+  getPipelineById(id: string) {
+    return this.customRenderingPipelines.get(id);
+  }
+
+  render(id: string, input: Landscape|Application) {
+    const pipeline = this.getPipelineById(id);
+    if(pipeline === undefined || pipeline.length <= 0)
+      return;
+    
+    let lastOutput: any = input;
+    for(let i = 0; i < pipeline.length; i++) {
+      lastOutput = pipeline[i](lastOutput);
+    }
   }
 
 }
