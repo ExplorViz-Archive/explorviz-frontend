18c9303c1e7c5acd80479cf6beceba487c84a37b
Malte Hansen <stu126317@mail.uni-kiel.de>
2019-12-30 18:58:30 +0100

    Remove rendering-core.js
    The functionality of the rendering-core is put into the landscape-rendering.
    This is useful to identify redundant or dead code.
    Additionally, it makes sense because  application rendering makes no use of
    the rendering-core anymore.

--- app/components/visualization/rendering/landscape-rendering.js
+++ app/components/visualization/rendering/landscape-rendering.js
@@ -1,7 +1,10 @@
-import RenderingCore from './rendering-core';
 import { inject as service } from '@ember/service';
 import { getOwner } from '@ember/application';
-
+import Component from '@ember/component';
+import Evented from '@ember/object/evented';
+import THREEPerformance from 'explorviz-frontend/utils/threejs-performance';
+import debugLogger from 'ember-debug-logger';
+import $ from 'jquery';
 import THREE from 'three';
 
 import applyKlayLayout from
@@ -30,27 +33,105 @@ import ImageLoader from 'explorviz-frontend/utils/three-image-loader';
 * @module explorviz
 * @submodule visualization.rendering
 */
-export default RenderingCore.extend({
+export default Component.extend(Evented, {
 
-  configuration: service("configuration"),
+  // No Ember generated container
+  tagName: '',
 
-  interaction: null,
-  labeler: null,
-  imageLoader: null,
+  debug: debugLogger(),
 
-  renderingService: service("rendering-service"),
+  state: null,
 
-  // There's already a property 'listener' in superclass RenderingCore
-  listeners2: null,
+  highlighter: service("visualization/application/highlighter"),
+  landscapeRepo: service("repos/landscape-repository"),
+  additionalData: service(),
+  configuration: service(),
+  reloadHandler: service(),
+  renderingService: service(),
+
+  currentUser: service(),
+
+  scene: null,
+  webglrenderer: null,
+  camera: null,
+
+  canvas: null,
+
+  font: null,
+  animationFrameId: null,
+
+  initDone: false,
+
+  initImport: true,
+
+  listeners: null,
+
+  threePerformance: null,
+
 
   // @Override
   /**
-   * Initialize listener functions, properties and utility objects
+   * This overridden Ember Component lifecycle hook enables calling
+   * ExplorViz's setup code for actual rendering and custom listeners.
    *
-   * @method initRendering
+   * @method didRender
+   */
+  didRender() {
+    this._super(...arguments);
+    this.initRendering();
+    this.initListener();
+  },
+
+
+  // @Override
+  /**
+   * This overridden Ember Component lifecycle hook enables calling
+   * ExplorViz's custom cleanup code.
+   *
+   * @method willDestroyElement
+   */
+  willDestroyElement() {
+    this._super(...arguments);
+    this.cleanup();
+  },
+
+
+  /**
+   * This function is called once on the didRender event. Inherit this function
+   * to call other important function, e.g. "initInteraction" as shown in
+   * {{#crossLink "Landscape-Rendering/initInteraction:method"}}{{/crossLink}}.
+   *
+   * @method initRenderings
    */
   initRendering() {
-    this._super(...arguments);
+    const self = this;
+
+    // Get size if outer ember div
+    const height = $('#rendering').innerHeight();
+    const width = $('#rendering').innerWidth();
+
+    const canvas = $('#threeCanvas')[0];
+
+    this.set('canvas', canvas);
+
+    this.set('scene', new THREE.Scene());
+    const backgroundColor = this.get('configuration.landscapeColors.background');
+    this.set('scene.background', new THREE.Color(backgroundColor));
+
+    this.set('camera', new THREE.PerspectiveCamera(75, width / height, 0.1, 1000));
+
+    this.set('webglrenderer', new THREE.WebGLRenderer({
+      antialias: true,
+      canvas: canvas
+    }));
+    this.get('webglrenderer').setPixelRatio(window.devicePixelRatio);
+    this.get('webglrenderer').setSize(width, height);
+
+    let showFpsCounter = this.get('currentUser').getPreferenceOrDefaultValue('flagsetting', 'showFpsCounter');
+
+    if (showFpsCounter) {
+      this.threePerformance = new THREEPerformance();
+    }
 
     this.debug("init landscape-rendering");
 
@@ -82,19 +163,165 @@ export default RenderingCore.extend({
       this.set('labeler', Labeler.create());
     }
 
-    const backgroundColor = this.get('configuration.landscapeColors.background');
-    this.set('scene.background', new THREE.Color(backgroundColor));
-
     this.initInteraction();
 
     const dirLight = new THREE.DirectionalLight();
     dirLight.position.set(30, 10, 20);
     this.get('scene').add(dirLight);
+
+    // Rendering loop //
+    function render() {
+      if (self.get('isDestroyed')) {
+        return;
+      }
+
+      const animationId = requestAnimationFrame(render);
+      self.set('animationFrameId', animationId);
+
+      if (showFpsCounter) {
+        self.threePerformance.threexStats.update(self.get('webglrenderer'));
+        self.threePerformance.stats.begin();
+      }
+
+      self.get('webglrenderer').render(self.get('scene'), self.get('camera'));
+
+      if (showFpsCounter) {
+        self.threePerformance.stats.end();
+      }
+    }
+
+    render();
+
+    ////////////////////
+
+    // Load font for labels and synchronously proceed with populating the scene
+    new THREE.FontLoader().load(
+      // resource URL
+      '/three.js/fonts/roboto_mono_bold_typeface.json',
+
+      // onLoad callback
+      function (font) {
+
+        if (self.isDestroyed)
+          return;
+
+        self.set('font', font);
+        self.debug("(THREE.js) font sucessfully loaded.");
+        self.set('initDone', true);
+        self.populateScene();
+      },
+
+      // onProgress callback
+      // function ( xhr ) {
+      //   self.debug("(THREE.js) font " + (xhr.loaded / xhr.total * 100) + "% loaded.");
+      // },
+
+      // onError callback
+      // function ( error ) {
+      //   self.debug("(THREE.js) Error when loading font!");
+      // }  
+    );
+
   },
 
-  // @Override
+
+  updateCanvasSize() {
+    const outerDiv = $('#vizspace')[0];
+
+    if (outerDiv) {
+      if (!this.get('camera') || !this.get('webglrenderer'))
+        return;
+
+      $('#threeCanvas').hide();
+
+      const height = Math.round($('#rendering').innerHeight());
+      const width = Math.round($('#rendering').innerWidth());
+
+      this.set('camera.aspect', width / height);
+      this.get('camera').updateProjectionMatrix();
+
+      this.get('webglrenderer').setSize(width, height);
+
+      this.onResized();
+
+      $('#threeCanvas').show();
+    }
+  },
+
+
+  initListener() {
+    this.set('listeners', new Set());
+
+    this.get('listeners').add([
+      'renderingService',
+      'reSetupScene',
+      () => {
+        this.onReSetupScene();
+      }
+    ]);
+
+    this.get('listeners').add([
+      'renderingService',
+      'resizeCanvas',
+      () => {
+        this.updateCanvasSize();
+      }
+    ]);
+
+    this.get('listeners').add([
+      'renderingService',
+      'moveCameraTo',
+      (emberModel) => {
+        this.onMoveCameraTo(emberModel);
+      }
+    ]);
+
+    this.get('listeners').add([
+      'landscapeRepo',
+      'updated',
+      () => {
+        this.onUpdated();
+      }
+    ]);
+
+    // Start subscriptions
+    this.get('listeners').forEach(([service, event, listenerFunction]) => {
+      this.get(service).on(event, listenerFunction);
+    });
+  },
+
+
+  /**
+   * This function is called when the willDestroyElement event is fired. Inherit this
+   * function to cleanup custom properties or unbind listener
+   * as shown in {{#crossLink "Landscape-Rendering"}}{{/crossLink}}.
+   *
+   * @method cleanup
+   */
   cleanup() {
-    this._super(...arguments);
+    cancelAnimationFrame(this.get('animationFrameId'));
+
+    this.set('scene', null);
+    this.set('webglrenderer', null);
+
+    // Clean up WebGL rendering context by forcing context loss
+    var gl = this.get('canvas').getContext('webgl');
+    gl.getExtension('WEBGL_lose_context').loseContext();
+
+    this.set('camera', null);
+
+    if (this.threePerformance) {
+      this.threePerformance.removePerformanceMeasurement();
+    }
+
+    // Unsubscribe from all services
+    this.get('listeners').forEach(([service, event, listenerFunction]) => {
+      this.get(service).off(event, listenerFunction);
+    });
+    this.set('listeners', null);
+
+    this.get('highlighter').unhighlightAll();
+    this.get('additionalData').emptyAndClose();
 
     this.debug("cleanup landscape rendering");
 
@@ -105,6 +332,67 @@ export default RenderingCore.extend({
     this.get('interaction').removeHandlers();
   },
 
+  /**
+   * Inherit this function to update the scene with a new renderingModel. It
+   * automatically removes every mesh from the scene and finally calls
+   * the (overridden) "populateScene" function. Add your custom code
+   * as shown in landscape-rendering.
+   *
+   * @method cleanAndUpdateScene
+   */
+  cleanAndUpdateScene() {
+    const scene = this.get('scene');
+
+    removeAllChildren(scene);
+
+    function removeAllChildren(entity) {
+      for (let i = entity.children.length - 1; i >= 0; i--) {
+        let child = entity.children[i];
+
+        removeAllChildren(child);
+
+        if (child.type !== 'AmbientLight' && child.type !== 'SpotLight' && child.type !== 'DirectionalLight') {
+          if (child.type !== 'Object3D') {
+            child.geometry.dispose();
+            child.material.dispose();
+          }
+          entity.remove(child);
+        }
+      }
+    }
+    this.populateScene();
+
+    this.debug("clean and populate landscape-rendering");
+
+    this.set('interaction.raycastObjects', this.get('scene.children'));
+  },
+
+
+  // Listener-Callbacks. Override in extending components
+
+  onReSetupScene() {
+    this.cleanAndUpdateScene();
+  },
+
+  onUpdated() {
+    this.cleanAndUpdateScene();
+  },
+
+  onResized() {
+    this.cleanAndUpdateScene();
+  },
+
+  onMoveCameraTo() { },
+
+  interaction: null,
+  labeler: null,
+  imageLoader: null,
+
+
+  // There's already a property 'listener' in superclass RenderingCore
+  listeners2: null,
+
+
   removeListeners() {
     // Unsubscribe from all services
     this.get('listeners2').forEach(([service, event, listenerFunction]) => {
@@ -113,20 +401,6 @@ export default RenderingCore.extend({
     this.set('listeners2', null);
   },
 
-
-  // @Override
-  /**
-   * TODO
-   *
-   * @method cleanAndUpdateScene
-   */
-  cleanAndUpdateScene() {
-    this._super(...arguments);
-
-    this.debug("clean and populate landscape-rendering");
-
-    this.set('interaction.raycastObjects', this.get('scene.children'));
-  },
 
   /**
    * The landscape is bound in the template, e.g., landscape=landscapeRepo.latestLandscape
@@ -347,7 +621,7 @@ export default RenderingCore.extend({
       .calculateLandscapeCenterAndZZoom(emberLandscape, this.get('webglrenderer'));
 
     // Update zoom if camera has not been moved by user
-    if (camera.position.z === 0){
+    if (camera.position.z === 0) {
       camera.position.z = center.z;
       camera.updateProjectionMatrix();
     }
