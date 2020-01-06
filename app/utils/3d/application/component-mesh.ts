34ea086bc7fe162b0b4f50dfcffef36d09a7f9a1
Malte Hansen <stu126317@mail.uni-kiel.de>
2020-01-06 17:04:39 +0100

    Add component mesh
    We introduce a specific mesh class for components which
    are used in application rendering. This effort is made to
    decouple view related data from the data model. Note: The
    foundation is not modelled as a component mesh because a foundation
    has no 'opened' state. Additionally, this allows to distinguish
    components from the foundation, e.g. for raycasting.

---
+++ app/utils/3d/application/component-mesh.ts
@@ -0,0 +1,23 @@
+import THREE from 'three';
+import Component from 'explorviz-frontend/models/component';
+import EntityMesh from '../entity-mesh';
+
+export default class ComponentMesh extends EntityMesh {
+
+  dataModel: Component;
+  opened: boolean;
+
+
+  constructor(layoutPos: THREE.Vector3, layoutHeight: number, layoutWidth: number, layoutDepth: number,
+    component: Component, color: THREE.Color, opened: boolean = false) {
+
+    super(layoutPos, layoutHeight, layoutWidth, layoutDepth);
+
+    let material = new THREE.MeshLambertMaterial({ color });
+    let geometry = new THREE.BoxGeometry(1, 1, 1);
+    this.geometry = geometry;
+    this.material = material;
+    this.opened = opened;
+    this.dataModel = component;
+  }
+}
