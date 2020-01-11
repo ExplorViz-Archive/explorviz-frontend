8f25ef55e061a4e6fd1ede38213f6f9fac505626
dan0x09 <stu126132@mail.uni-kiel.de>
2020-01-11 13:34:30 +0100

    Remove host setting from .ember-cli.js
    Setting the host address to 0.0.0.0 makes 'ember test' fail with the
    error that testem might not be loaded (at least on Windows).

--- .ember-cli.js
+++ .ember-cli.js
@@ -5,8 +5,7 @@ module.exports = {
 
     Setting `disableAnalytics` to true will prevent any data from being sent.
   */
-  "disableAnalytics": true,
-  "host": "0.0.0.0"
+  "disableAnalytics": true
 }
 
 const { setEdition } = require('@ember/edition-utils');
