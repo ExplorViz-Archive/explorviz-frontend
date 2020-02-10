2aef8e7724ffffc693f8485c9e080df0a4aff095
dan0x09 <danielkoenig95@web.de>
2020-02-10 18:19:36 +0100

    Fix code styling errors in multiple files

--- app/components/visualization/rendering/popups/application-popups/component-popup.ts
+++ app/components/visualization/rendering/popups/application-popups/component-popup.ts
@@ -1,38 +1,37 @@
-import GlimmerComponent from "@glimmer/component";
-import Component from "explorviz-frontend/models/component";
+import GlimmerComponent from '@glimmer/component';
+import Component from 'explorviz-frontend/models/component';
 
 interface Args {
   component: Component
 }
 
 export default class ComponentPopup extends GlimmerComponent<Args> {
-
   get name() {
     return this.args.component.get('name');
   }
 
   get clazzCount() {
-    return this._getClazzesCount(this.args.component);
+    return this.getClazzesCount(this.args.component);
   }
 
   get packageCount() {
-    return this._getPackagesCount(this.args.component);
+    return this.getPackagesCount(this.args.component);
   }
 
-  _getClazzesCount(component:Component) : number {
+  getClazzesCount(component: Component): number {
     let result = component.get('clazzes').get('length');
     const children = component.get('children');
     children.forEach((child) => {
-      result += this._getClazzesCount(child);
+      result += this.getClazzesCount(child);
     });
     return result;
   }
 
-  _getPackagesCount(component:Component) : number {
+  getPackagesCount(component: Component): number {
     let result = component.get('children').get('length');
     const children = component.get('children');
     children.forEach((child) => {
-      result += this._getPackagesCount(child);
+      result += this.getPackagesCount(child);
     });
     return result;
   }
