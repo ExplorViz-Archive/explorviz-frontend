4c854b2ab5e5bf6a4e623a695e8637ea8953743a
dan0x09 <stu126132@mail.uni-kiel.de>
2020-03-31 20:54:17 +0200

    Add model reducer for landscapes to be used in klay

---
+++ tests/unit/utils/landscape-rendering/model-reducer-test.ts
@@ -0,0 +1,11 @@
+import landscapeRenderingModelReducer from 'explorviz-frontend/utils/landscape-rendering/model-reducer';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | landscape-rendering/model-reducer', function(hooks) {
+
+  // Replace this with your real tests.
+  test('it works', function(assert) {
+    let result = landscapeRenderingModelReducer();
+    assert.ok(result);
+  });
+});
