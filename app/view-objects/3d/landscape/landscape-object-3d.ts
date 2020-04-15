565e0f647b9a6f9de0aaa3255b4ebfa275679f76
Malte Hansen <malte.hanseat@gmail.com>
2020-04-16 00:55:31 +0200

    Further comment LandscapeObject3D

--- app/view-objects/3d/landscape/landscape-object-3d.ts
+++ app/view-objects/3d/landscape/landscape-object-3d.ts
@@ -1,6 +1,7 @@
 import THREE from 'three';
 import Landscape from 'explorviz-frontend/models/landscape';
 import MinMaxRectangle from 'explorviz-frontend/view-objects/layout-models/min-max-rectangle';
+import PlaneLayout from 'explorviz-frontend/view-objects/layout-models/plane-layout';
 import NodeGroup from 'explorviz-frontend/models/nodegroup';
 import Node from 'explorviz-frontend/models/node';
 import SystemMesh from './system-mesh';
@@ -107,10 +108,7 @@ export default class LandscapeObject3D extends THREE.Object3D {
     return openEntityIds;
   }
 
-  /**
-   * Returns a rectangle which represents the size of the landscape
-   */
-  getMinMaxRect() {
+  getMinMaxRect(modelIdToLayout: Map<string, PlaneLayout>) {
     // Rectangle which can be used to find smallest and greatest x/y coordinates
     const rect = new MinMaxRectangle();
 
@@ -121,27 +119,26 @@ export default class LandscapeObject3D extends THREE.Object3D {
       rect.setMinValues(0, 0);
       rect.setMaxValues(1, 1);
     } else {
+      // Check systems for new min/max position
       systems.forEach((system: any) => {
-        const systemMesh = this.getMeshbyModelId(system.id);
-        // Check positions of each system
-        if (systemMesh instanceof SystemMesh) {
-          rect.setMinMaxFromLayout(systemMesh.layout);
+        const systemLayout = modelIdToLayout.get(system.get('id'));
+        if (systemLayout) {
+          rect.setMinMaxFromLayout(systemLayout);
         }
 
+        // Check nodegroups for new min/max position
         const nodegroups = system.get('nodegroups');
         nodegroups.forEach((nodegroup: NodeGroup) => {
           const nodes = nodegroup.get('nodes');
           nodes.forEach((node: Node) => {
-            const nodeMesh = this.getMeshbyModelId(node.id);
-            // Check positions of each Node
-            if (nodeMesh instanceof NodeMesh) {
-              rect.setMinMaxFromLayout(nodeMesh.layout);
+            const nodeLayout = modelIdToLayout.get(node.get('id'));
+            if (nodeLayout) {
+              rect.setMinMaxFromLayout(nodeLayout);
             }
           });
         });
       });
     }
-
     return rect;
   }
 }
