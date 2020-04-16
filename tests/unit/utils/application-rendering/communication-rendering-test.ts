0010533775068fed53b17850e69d581d6aaa3a87
dan0x09 <stu126132@mail.uni-kiel.de>
2020-04-16 15:21:54 +0200

    Add missing test files for existing utilities

---
+++ tests/unit/utils/application-rendering/communication-rendering-test.ts
@@ -0,0 +1,10 @@
+//import applicationRenderingCommunicationRendering from 'explorviz-frontend/utils/application-rendering/communication-rendering';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | application rendering/communication rendering');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  //let result = applicationRenderingCommunicationRendering();
+  assert.equal(true,true);
+});
---
+++ tests/unit/utils/application-rendering/entity-rendering-test.ts
@@ -0,0 +1,10 @@
+//import applicationRenderingEntityRendering from 'explorviz-frontend/utils/application-rendering/entity-rendering';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | application rendering/entity rendering');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  //let result = applicationRenderingEntityRendering();
+  assert.equal(true,true);
+});
