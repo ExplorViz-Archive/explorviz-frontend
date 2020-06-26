a8daa7af1e61d6fb48ee822ffb43e9ceb4f5ace3
dan0x09 <stu126132@mail.uni-kiel.de>
2020-06-26 13:52:28 +0200

    Adapt navbar-adv comonent to use new auth service to display username

--- app/components/page-setup/navbar-adv.ts
+++ app/components/page-setup/navbar-adv.ts
@@ -1,8 +1,18 @@
 import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
 import Component from '@glimmer/component';
 import CurrentUser from 'explorviz-frontend/services/current-user';
+import Auth from 'explorviz-frontend/services/auth';
 
 export default class NavbarAdvanced extends Component {
   @service('current-user')
   currentUser!: CurrentUser;
+
+  @service('auth')
+  auth!: Auth;
+
+  @action
+  logout() {
+    this.auth.logout();
+  }
 }
