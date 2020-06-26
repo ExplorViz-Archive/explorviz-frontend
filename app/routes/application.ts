7172db7e73281162a8223032a9b278d397c0187c
dan0x09 <stu126132@mail.uni-kiel.de>
2020-06-26 13:58:49 +0200

    Use auth service in application route to determine navbar visibility

--- app/routes/application.ts
+++ app/routes/application.ts
@@ -1,60 +1,15 @@
-import { action } from '@ember/object';
 import Route from '@ember/routing/route';
-// @ts-ignore
-import ApplicationRouteMixin from 'ember-simple-auth-auth0/mixins/application-route-mixin';
-import ApplicationController from 'explorviz-frontend/controllers/application';
-import RSVP from 'rsvp';
+import { inject as service } from '@ember/service';
+import Auth from 'explorviz-frontend/services/auth';
 
 /**
  * TODO
  *
- * @class Application-Route
- * @extends Ember.Route
+ * @class ApplicationRoute
+ * @extends Route
  */
 // @ts-ignore
-export default class ApplicationRoute extends Route.extend(ApplicationRouteMixin) {
-  routeAfterAuthentication = 'visualization';
-
-  beforeSessionExpired() {
-    // Do custom async logic here, e.g. notify
-    // the user that they are about to be logged out.
-
-    return  RSVP.resolve()
-  }
-
-  async beforeModel() {
-    if (this.session.data.authenticated.accessToken) {
-      const user = {
-        username: this.session.data.authenticated.profile.nickname,
-        token: this.session.data.authenticated.accessToken,
-        roles: ['admin'],
-        id: this.session.data.authenticated.profile.sub,
-      };
-
-      this.store.createRecord('user', user);
-    }
-
-    await (this.controllerFor('application') as ApplicationController).loadUserAndSettings.perform();
-  }
-
-  async sessionAuthenticated() {
-    const user = {
-      username: this.session.data.authenticated.profile.nickname,
-      token: this.session.data.authenticated.accessToken,
-      roles: ['admin'],
-      id: this.session.data.authenticated.profile.sub,
-    };
-
-    this.store.createRecord('user', user);
-
-    await (this.controllerFor('application') as ApplicationController).loadUserAndSettings.perform();
-
-
-    super.sessionAuthenticated(...arguments);
-  }
-
-  @action
-  logout() {
-    this.session.invalidate({ message: 'Logout successful' });
-  }
+export default class ApplicationRoute extends Route {
+  @service
+  auth!: Auth;
 }
