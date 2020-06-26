0c06a3c13e454f2ac7aca45d658545e73c6b5165
dan0x09 <stu126132@mail.uni-kiel.de>
2020-06-26 13:44:59 +0200

    Add configuration file for auth0 variables

--- config/auth0-variables.js
+++ config/auth0-variables.js
@@ -2,6 +2,6 @@ module.exports = {
   clientId: '7p8yE0P22rXDog2z1wHTp5xvgUbMgoK2',
   domain: 'dev-0kw21a7w.auth0.com',
   callbackUrl: 'http://localhost:4200/callback',
-  logoutReturnUrl: 'http://localhost:4200',
+  logoutReturnUrl: 'http://localhost:4200/',
   routeAfterLogin: 'visualization',
-}
+};
