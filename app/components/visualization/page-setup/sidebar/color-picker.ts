0bd42d25a9a48d8176e8708949517356e416ff11
Malte Hansen <stu126317@mail.uni-kiel.de>
2020-07-23 16:36:01 +0200

    Use bootstrap-colorpicker input elements
    The standard html input element for colors triggers no events while
    selecting a color. An event is only triggered after a user saves the
    selection (either by clicking a button or clicking outside the input
    element).
    This commit makes use of the bootstrap-colorpicker addon which offers
    a highly customizable color input. Additionally, the
    bootstrap-colorpicker offers events to register a change within the
    color selection. This is used to trigger a real time update of the view.

--- app/components/visualization/page-setup/sidebar/color-picker.ts
+++ app/components/visualization/page-setup/sidebar/color-picker.ts
@@ -1,8 +1,7 @@
-import { action, set } from '@ember/object';
+import { action } from '@ember/object';
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
-import Configuration from 'explorviz-frontend/services/configuration';
-import { tracked } from '@glimmer/tracking';
+import Configuration, { LandscapeColors, ApplicationColors } from 'explorviz-frontend/services/configuration';
 
 interface Args {
   isLandscapeView: Boolean
@@ -14,37 +13,6 @@ export default class ColorPicker extends Component<Args> {
   @service('configuration')
   configuration!: Configuration;
 
-  // Bound to color inputs
-  @tracked
-  appColors = {
-    foundation: '#c7c7c7', // grey
-    componentOdd: '#169e2b', // dark green
-    componentEven: '#00bb41', // light green
-    clazz: '#3e14a0', // purple-blue
-    highlightedEntity: '#ff0000', // red
-    componentText: '#ffffff', // white
-    clazzText: '#ffffff', // white
-    foundationText: '#000000', // black
-    communication: '#f49100', // orange
-    communicationArrow: '#000000', // black
-    background: '#ffffff', // white
-  };
-
-  // Bound to color inputs
-  @tracked
-  landscapeColors = {
-    system: '#c7c7c7', // grey
-    nodegroup: '#169e2b', // dark green
-    node: '#00bb41', // green
-    application: '#3e14a0', // purple-blue
-    communication: '#f49100', // orange
-    systemText: '#000000', // black
-    nodeText: '#ffffff', // white
-    applicationText: '#ffffff', // white
-    collapseSymbol: '#000000', // black
-    background: '#ffffff', // white
-  };
-
   @action
   applyColorScheme(scheme: string) {
     if (scheme === 'default') {
@@ -54,46 +22,59 @@ export default class ColorPicker extends Component<Args> {
     }
   }
 
-  /**
-   * Updates the colors in the configuration with respect to the color input values.
-   * Triggers a update of the view.
-   */
-  @action
-  updateColors() {
-    // Update application color configuration with colorpicker values
-    if (this.args.isLandscapeView) {
-      this.configuration.landscapeColors.system.set(this.landscapeColors.system);
-      this.configuration.landscapeColors.nodegroup.set(this.landscapeColors.nodegroup);
-      this.configuration.landscapeColors.node.set(this.landscapeColors.node);
-      this.configuration.landscapeColors.application.set(this.landscapeColors.application);
-      this.configuration.landscapeColors.communication.set(this.landscapeColors.communication);
-      this.configuration.landscapeColors.systemText.set(this.landscapeColors.systemText);
-      this.configuration.landscapeColors.nodeText.set(this.landscapeColors.nodeText);
-      this.configuration.landscapeColors.applicationText.set(this.landscapeColors.applicationText);
-      this.configuration.landscapeColors.collapseSymbol.set(this.landscapeColors.collapseSymbol);
-      this.configuration.landscapeColors.background.set(this.landscapeColors.background);
-    // Update landscape color configuration with colorpicker values
-    } else {
-      this.configuration.applicationColors.foundation.set(this.appColors.foundation);
-      this.configuration.applicationColors.componentOdd.set(this.appColors.componentOdd);
-      this.configuration.applicationColors.componentEven.set(this.appColors.componentEven);
-      this.configuration.applicationColors.clazz.set(this.appColors.clazz);
-      this.configuration.applicationColors.highlightedEntity.set(this.appColors.highlightedEntity);
-      this.configuration.applicationColors.componentText.set(this.appColors.componentText);
-      this.configuration.applicationColors.clazzText.set(this.appColors.clazzText);
-      this.configuration.applicationColors.foundationText.set(this.appColors.foundationText);
-      this.configuration.applicationColors.communication.set(this.appColors.communication);
-      this.configuration.applicationColors.communicationArrow
-        .set(this.appColors.communicationArrow);
-      this.configuration.applicationColors.background.set(this.appColors.background);
-    }
-
-    this.args.updateView();
-  }
-
   @action
   close(this: ColorPicker) {
     this.args.removeComponent('color-picker');
+  }
+
+  @action
+  setupLandscapeColorpicker(colorName: keyof LandscapeColors, element: HTMLElement) {
+    const configColor = this.configuration.landscapeColors[colorName];
+    this.setupColorpicker(element, configColor);
+  }
+
+  @action
+  setupApplicationColorPicker(colorName: keyof ApplicationColors, element: HTMLElement) {
+    const configColor = this.configuration.applicationColors[colorName];
+    this.setupColorpicker(element, configColor);
+  }
+
+  /**
+   * Initilizes a given colorpicker element with a passed color.
+   * Additionally, the color update event is handled.
+   *
+   * @param element The HTML colorpicker element
+   * @param configColor Reference to the respective color in the configuration service
+   */
+  setupColorpicker(element: HTMLElement, configColor: THREE.Color) {
+    // eslint-disable-next-line
+    $(`#${element.id}`)
+    // @ts-ignore
+      .colorpicker(
+        {
+          color: configColor.getHexString(),
+          format: 'hex',
+          useAlpha: false,
+        },
+      ).on('colorpickerChange', (e: any) => {
+        const inputColor = e.color.toHexString();
+        configColor.set(inputColor);
+
+        this.args.updateView();
+      });
+  }
+
+  /**
+   * Marks the colors in configuration as updated and triggers a view update.
+   */
+  updateView() {
+    if (this.args.isLandscapeView) {
+      this.configuration.notifyPropertyChange('landscapeColors');
+    } else {
+      this.configuration.notifyPropertyChange('applicationColors');
+    }
+
+    this.args.updateView();
   }
 
   /**
@@ -102,31 +83,34 @@ export default class ColorPicker extends Component<Args> {
    */
   applyDefaultColors() {
     if (this.args.isLandscapeView) {
-      set(this.landscapeColors, 'system', '#c7c7c7'); // grey
-      set(this.landscapeColors, 'nodegroup', '#169e2b'); // dark green
-      set(this.landscapeColors, 'node', '#00bb41'); // green
-      set(this.landscapeColors, 'application', '#3e14a0'); // purple-blue
-      set(this.landscapeColors, 'communication', '#f49100'); // orange
-      set(this.landscapeColors, 'systemText', '#000000'); // black
-      set(this.landscapeColors, 'nodeText', '#ffffff'); // white
-      set(this.landscapeColors, 'applicationText', '#ffffff'); // white
-      set(this.landscapeColors, 'collapseSymbol', '#000000'); // black
-      set(this.landscapeColors, 'background', '#ffffff'); // white
+      const { landscapeColors } = this.configuration;
+
+      landscapeColors.system.set('#c7c7c7'); // grey
+      landscapeColors.nodegroup.set('#169e2b'); // dark green
+      landscapeColors.node.set('#00bb41'); // green
+      landscapeColors.application.set('#3e14a0'); // purple-blue
+      landscapeColors.communication.set('#f49100'); // orange
+      landscapeColors.systemText.set('#000000'); // black
+      landscapeColors.nodeText.set('#ffffff'); // white
+      landscapeColors.applicationText.set('#ffffff'); // white
+      landscapeColors.collapseSymbol.set('#000000'); // black
+      landscapeColors.background.set('#ffffff'); // white
     } else {
-      set(this.appColors, 'foundation', '#c7c7c7'); // grey
-      set(this.appColors, 'componentOdd', '#169e2b'); // dark green
-      set(this.appColors, 'componentEven', '#00bb41'); // light green
-      set(this.appColors, 'clazz', '#3e14a0'); // purple-blue
-      set(this.appColors, 'highlightedEntity', '#ff0000'); // red
-      set(this.appColors, 'componentText', '#ffffff'); // white
-      set(this.appColors, 'clazzText', '#ffffff'); // white
-      set(this.appColors, 'foundationText', '#000000'); // black
-      set(this.appColors, 'communication', '#f49100'); // orange
-      set(this.appColors, 'communicationArrow', '#000000'); // black
-      set(this.appColors, 'background', '#ffffff'); // white
+      const { applicationColors } = this.configuration;
+      applicationColors.foundation.set('#c7c7c7'); // grey
+      applicationColors.componentOdd.set('#169e2b'); // dark green
+      applicationColors.componentEven.set('#00bb41'); // light green
+      applicationColors.clazz.set('#3e14a0'); // purple-blue
+      applicationColors.highlightedEntity.set('#ff0000'); // red
+      applicationColors.componentText.set('#ffffff'); // white
+      applicationColors.clazzText.set('#ffffff'); // white
+      applicationColors.foundationText.set('#000000'); // black
+      applicationColors.communication.set('#f49100'); // orange
+      applicationColors.communicationArrow.set('#000000'); // black
+      applicationColors.background.set('#ffffff'); // white
     }
 
-    this.updateColors();
+    this.updateView();
   }
 
   /**
@@ -135,30 +119,34 @@ export default class ColorPicker extends Component<Args> {
    */
   applyVisuallyImpairedColors() {
     if (this.args.isLandscapeView) {
-      set(this.landscapeColors, 'system', '#c7c7c7'); // grey
-      set(this.landscapeColors, 'nodegroup', '#015a6e'); // blue
-      set(this.landscapeColors, 'node', '#0096be'); // light blue
-      set(this.landscapeColors, 'application', '#5f5f5f'); // dark grey
-      set(this.landscapeColors, 'communication', '#f49100'); // orange
-      set(this.landscapeColors, 'systemText', '#000000'); // black
-      set(this.landscapeColors, 'nodeText', '#ffffff'); // white
-      set(this.landscapeColors, 'applicationText', '#ffffff'); // white
-      set(this.landscapeColors, 'collapseSymbol', '#000000'); // black
-      set(this.landscapeColors, 'background', '#ffffff'); // white
+      const { landscapeColors } = this.configuration;
+
+      landscapeColors.system.set('#c7c7c7'); // grey
+      landscapeColors.nodegroup.set('#015a6e'); // dark green
+      landscapeColors.node.set('#0096be'); // green
+      landscapeColors.application.set('#5f5f5f'); // purple-blue
+      landscapeColors.communication.set('#f49100'); // orange
+      landscapeColors.systemText.set('#000000'); // black
+      landscapeColors.nodeText.set('#ffffff'); // white
+      landscapeColors.applicationText.set('#ffffff'); // white
+      landscapeColors.collapseSymbol.set('#000000'); // black
+      landscapeColors.background.set('#ffffff'); // white
     } else {
-      set(this.appColors, 'foundation', '#c7c7c7'); // grey
-      set(this.appColors, 'componentOdd', '#015a6e'); // blue
-      set(this.appColors, 'componentEven', '#0096be'); // light blue
-      set(this.appColors, 'clazz', '#5f5f5f'); // dark grey
-      set(this.appColors, 'highlightedEntity', '#ff0000'); // red
-      set(this.appColors, 'componentText', '#ffffff'); // white
-      set(this.appColors, 'clazzText', '#ffffff'); // white
-      set(this.appColors, 'foundationText', '#000000'); // black
-      set(this.appColors, 'communication', '#f49100'); // orange
-      set(this.appColors, 'communicationArrow', '#000000'); // black
-      set(this.appColors, 'background', '#ffffff'); // white
+      const { applicationColors } = this.configuration;
+
+      applicationColors.foundation.set('#c7c7c7'); // grey
+      applicationColors.componentOdd.set('#015a6e'); // blue
+      applicationColors.componentEven.set('#0096be'); // light blue
+      applicationColors.clazz.set('#5f5f5f'); // dark grey
+      applicationColors.highlightedEntity.set('#ff0000'); // red
+      applicationColors.componentText.set('#ffffff'); // white
+      applicationColors.clazzText.set('#ffffff'); // white
+      applicationColors.foundationText.set('#000000'); // black
+      applicationColors.communication.set('#f49100'); // orange
+      applicationColors.communicationArrow.set('#000000'); // black
+      applicationColors.background.set('#ffffff'); // white
     }
 
-    this.updateColors();
+    this.updateView();
   }
 }
