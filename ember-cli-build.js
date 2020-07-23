677f0aa6e8d8c67e21f1863206e529ecbdf626d7
Malte Hansen <stu126317@mail.uni-kiel.de>
2020-07-23 16:34:01 +0200

    Import bootstrap-colorpicker (css and js) into ember app

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,52 +1,51 @@
-/*jshint node:true*/
+/* jshint node:true */
 /* global require, module */
-var sass = require('sass');
-var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const sass = require('sass');
+const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-module.exports = function(defaults) {
-
-  var app = new EmberApp(defaults, {
+module.exports = (defaults) => {
+  const app = new EmberApp(defaults, {
     octicons: {
       // load selected icons for popup-handler since it does not (yet) use components / templates
-      icons: ['arrow-right', 'arrow-both', 'code', 'list-ordered', 'clock', 'tools']
+      icons: ['arrow-right', 'arrow-both', 'code', 'list-ordered', 'clock', 'tools'],
     },
 
     // Default implementation for ember-cli-sass
     sassOptions: {
-      implementation: sass
+      implementation: sass,
     },
 
     svgJar: {
       sourceDirs: [
         'public', // default SVGJar lookup directory
-        'node_modules/octicons/build/svg'
-      ]
+        'node_modules/octicons/build/svg',
+      ],
     },
 
     'ember-cli-babel': {
-      includePolyfill: true
+      includePolyfill: true,
     },
 
     fingerprint: {
-      exclude: ['images']
+      exclude: ['images'],
     },
 
     nodeModulesToVendor: [
       // add node_modules that you need in vendor modules
       // See: https://www.npmjs.com/package/ember-cli-node-modules-to-vendor
-      'node_modules/three/build'
+      'node_modules/three/build',
     ],
 
     'ember-bootstrap': {
-      'bootstrapVersion': 4,
-      'importBootstrapFont': false,
-      'importBootstrapCSS': false
-    }
+      bootstrapVersion: 4,
+      importBootstrapFont: false,
+      importBootstrapCSS: false,
+    },
   });
 
   // export for threex.dynamictexture
-  app.import('vendor/three.min.js',{
-    prepend: true
+  app.import('vendor/three.min.js', {
+    prepend: true,
   });
 
   app.import('vendor/layout/klay.js');
@@ -62,6 +61,9 @@ module.exports = function(defaults) {
   app.import('vendor/eventsource-polyfill/eventsource.min.js');
 
   app.import('node_modules/bootstrap/dist/js/bootstrap.min.js');
+
+  app.import('node_modules/bootstrap-colorpicker/dist/css/bootstrap-colorpicker.min.css');
+  app.import('node_modules/bootstrap-colorpicker/dist/js/bootstrap-colorpicker.min.js');
 
   return app.toTree();
 };
