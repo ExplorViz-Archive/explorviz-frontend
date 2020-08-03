0880c57112e22d594ecebfe9420a6e8452625e54
Malte Hansen <stu126317@mail.uni-kiel.de>
2020-08-03 20:07:16 +0200

    Add first draft of vr-rendering component

--- lib/virtual-reality/addon/components/vr-rendering.ts
+++ lib/virtual-reality/addon/components/vr-rendering.ts
@@ -1,5 +1,146 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+import debugLogger from 'ember-debug-logger';
+import THREE from 'three';
+import Configuration from 'explorviz-frontend/services/configuration';
 
-interface VrRenderingArgs {}
+// Declare globals
+/* global VRButton */
 
-export default class VrRendering extends Component<VrRenderingArgs> {}
+interface Args {}
+
+export default class VrRendering extends Component<Args> {
+  // #region CLASS FIELDS AND GETTERS
+
+  @service('configuration')
+  configuration!: Configuration;
+
+  debug = debugLogger('VrRendering');
+
+  canvas!: HTMLCanvasElement;
+
+  scene!: THREE.Scene;
+
+  camera!: THREE.PerspectiveCamera;
+
+  renderer!: THREE.WebGLRenderer;
+
+  // #endregion CLASS FIELDS AND GETTERS
+
+  // #region COMPONENT AND SCENE INITIALIZATION
+
+  constructor(owner: any, args: Args) {
+    super(owner, args);
+    this.debug('Constructor called');
+  }
+
+  @action
+  canvasInserted(canvas: HTMLCanvasElement) {
+    this.debug('Canvas inserted');
+
+    this.canvas = canvas;
+
+    canvas.oncontextmenu = (e) => {
+      e.preventDefault();
+    };
+  }
+
+  @action
+  async outerDivInserted(outerDiv: HTMLElement) {
+    this.debug('Outer Div inserted');
+
+    this.initThreeJs();
+    outerDiv.appendChild(VRButton.createButton(this.renderer));
+    // this.initInteraction();
+    this.render();
+
+    this.resize(outerDiv);
+  }
+
+  /**
+     * Calls all three related init functions and adds the three
+     * performance panel if it is activated in user settings
+     */
+  initThreeJs() {
+    this.initScene();
+    this.initCamera();
+    this.initRenderer();
+    this.initLights();
+  }
+
+  /**
+     * Creates a scene, its background and adds a landscapeObject3D to it
+     */
+  initScene() {
+    this.scene = new THREE.Scene();
+    this.scene.background = new THREE.Color(this.configuration.landscapeColors.background);
+    this.debug('Scene created');
+  }
+
+  /**
+     * Creates a PerspectiveCamera according to canvas size and sets its initial position
+     */
+  initCamera() {
+    const { width, height } = this.canvas;
+    this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+    this.camera.position.set(0, 0, 100);
+    this.debug('Camera added');
+  }
+
+  /**
+     * Initiates a WebGLRenderer
+     */
+  initRenderer() {
+    const { width, height } = this.canvas;
+    this.renderer = new THREE.WebGLRenderer({
+      antialias: true,
+      canvas: this.canvas,
+    });
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.setSize(width, height);
+    this.renderer.xr.enabled = true;
+    this.debug('Renderer set up');
+  }
+
+  /**
+     * Creates a SpotLight and an AmbientLight and adds it to the scene
+     */
+  initLights() {
+    const spotLight = new THREE.SpotLight(0xffffff, 0.5, 1000, 1.56, 0, 0);
+    spotLight.position.set(100, 100, 100);
+    spotLight.castShadow = false;
+    this.scene.add(spotLight);
+
+    const light = new THREE.AmbientLight(new THREE.Color(0.65, 0.65, 0.65));
+    this.scene.add(light);
+    this.debug('Lights added');
+  }
+
+  /**
+   * Main rendering function
+   */
+  render() {
+    if (this.isDestroyed) { return; }
+
+    this.renderer.setAnimationLoop(this.render.bind(this));
+    this.renderer.render(this.scene, this.camera);
+  }
+
+  /**
+   * Call this whenever the canvas is resized. Updated properties of camera
+   * and renderer.
+   *
+   * @param outerDiv HTML element containing the canvas
+   */
+  @action
+  resize(outerDiv: HTMLElement) {
+    const width = Number(outerDiv.clientWidth);
+    const height = Number(outerDiv.clientHeight);
+
+    // Update renderer and camera according to new canvas size
+    this.renderer.setSize(width, height);
+    this.camera.aspect = width / height;
+    this.camera.updateProjectionMatrix();
+  }
+}
