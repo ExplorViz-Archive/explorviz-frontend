b4298fefd10e6c88744462b7035d68f9804f26ad
Malte Hansen <malte.hanseat@gmail.com>
2020-09-09 12:08:08 +0200

    Enable user to always switch to virtual-reality
    The user should be able to enter the virtual reality rendering
    even though WebXR might not be enabled. This is because the virtual
    reality component can also be used without the proper hardware and
    is a good place to check if the connected VR hardware is working.

--- app/components/visualization/page-setup/navbar/vr-toggler.ts
+++ app/components/visualization/page-setup/navbar/vr-toggler.ts
@@ -1,39 +1,13 @@
 import { action } from '@ember/object';
 import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
 
 interface VrTogglerArgs {
   switchToVR: () => void;
 }
 
 export default class VrToggler extends Component<VrTogglerArgs> {
-  @tracked
-  xrActive: Boolean;
-
-  @tracked
-  tooltip: string;
-
-  constructor(owner: any, args: VrTogglerArgs) {
-    super(owner, args);
-
-    this.xrActive = false;
-    this.tooltip = 'WEBXR NOT AVAILABLE';
-
-    if ('xr' in navigator) {
-      // @ts-ignore
-      navigator.xr.isSessionSupported('immersive-vr').then((supported: boolean) => {
-        if (supported) {
-          this.xrActive = true;
-          this.tooltip = 'Switch to Virtual Reality';
-        }
-      });
-    }
-  }
-
   @action
   openVrMode() {
-    if (this.xrActive) {
-      this.args.switchToVR();
-    }
+    this.args.switchToVR();
   }
 }
