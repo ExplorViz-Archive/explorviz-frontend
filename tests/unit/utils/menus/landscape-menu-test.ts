99303c149027e3b4b6ac66c727b44b1c0b7db490
dan0x09 <danielkoenig95@web.de>
2020-09-11 15:05:47 +0200

    Add VR menu for adjusting landscape's position and orientation

---
+++ tests/unit/utils/menus/landscape-menu-test.ts
@@ -0,0 +1,11 @@
+import menusLandscapeMenu from 'explorviz-frontend/utils/menus/landscape-menu';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | menus/landscape-menu', function(hooks) {
+
+  // Replace this with your real tests.
+  test('it works', function(assert) {
+    let result = menusLandscapeMenu();
+    assert.ok(result);
+  });
+});
