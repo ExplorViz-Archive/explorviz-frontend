82bc3bfff77d97995b4fc3c149d553430517cfbf
dan0x09 <danielkoenig95@web.de>
2020-09-11 15:07:01 +0200

    Add main-menu which enables access to sub-menus s.a. camera height menu

---
+++ tests/unit/utils/menus/main-menu-test.ts
@@ -0,0 +1,11 @@
+import menusMainMenu from 'explorviz-frontend/utils/menus/main-menu';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | menus/main-menu', function(hooks) {
+
+  // Replace this with your real tests.
+  test('it works', function(assert) {
+    let result = menusMainMenu();
+    assert.ok(result);
+  });
+});
