568379fc2c7d5ce66368fdbdf6dda86ed5b7f0c8
dan0x09 <danielkoenig95@web.de>
2020-09-11 15:04:09 +0200

    Add menu to VR for adjusting user's height

---
+++ tests/unit/utils/menus/camera-menu-test.ts
@@ -0,0 +1,11 @@
+import menusCameraMenu from 'explorviz-frontend/utils/menus/camera-menu';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | menus/camera-menu', function(hooks) {
+
+  // Replace this with your real tests.
+  test('it works', function(assert) {
+    let result = menusCameraMenu();
+    assert.ok(result);
+  });
+});
