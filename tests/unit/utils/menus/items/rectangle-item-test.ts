f05eef75635b4ca9262f3ca8e498866040ace3cf
dan0x09 <danielkoenig95@web.de>
2020-09-11 14:58:33 +0200

    Add a rectangle item for VR menus to draw rectangle shapes inside menus

---
+++ tests/unit/utils/menus/items/rectangle-item-test.ts
@@ -0,0 +1,11 @@
+import menusItemsRectangleItem from 'explorviz-frontend/utils/menus/items/rectangle-item';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | menus/items/rectangle-item', function(hooks) {
+
+  // Replace this with your real tests.
+  test('it works', function(assert) {
+    let result = menusItemsRectangleItem();
+    assert.ok(result);
+  });
+});
