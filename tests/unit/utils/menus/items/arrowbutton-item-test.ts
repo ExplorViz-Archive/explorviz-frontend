37508c1d241f7998628fd23bd4e3c4ef87386a73
dan0x09 <danielkoenig95@web.de>
2020-09-18 21:43:47 +0200

    In VR, add arrow shaped menu items

---
+++ tests/unit/utils/menus/items/arrowbutton-item-test.ts
@@ -0,0 +1,11 @@
+import menusItemsArrowbuttonItem from 'explorviz-frontend/utils/menus/items/arrowbutton-item';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | menus/items/arrowbutton-item', function(hooks) {
+
+  // Replace this with your real tests.
+  test('it works', function(assert) {
+    let result = menusItemsArrowbuttonItem();
+    assert.ok(result);
+  });
+});
