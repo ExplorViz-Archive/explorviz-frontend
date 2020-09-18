6989f5c42a6cfa04d72a0d45dacda01db01f7533
dan0x09 <danielkoenig95@web.de>
2020-09-18 21:50:42 +0200

    In VR, add add arrow buttons to camera menu

--- lib/virtual-reality/addon/utils/menus/camera-menu.ts
+++ lib/virtual-reality/addon/utils/menus/camera-menu.ts
@@ -1,6 +1,7 @@
 import TextItem from './items/text-item';
 import BaseMenu from './base-menu';
 import TextbuttonItem from './items/textbutton-item';
+import ArrowbuttonItem from './items/arrowbutton-item';
 
 export default class CameraMenu extends BaseMenu {
   constructor(openMainMenu: () => void, userPosition: THREE.Vector3) {
@@ -11,10 +12,10 @@ export default class CameraMenu extends BaseMenu {
     const textItem = new TextItem(userPosition.y.toFixed(2), 'camera_height', '#ffffff', { x: 256, y: 202 }, 28, 'center');
     this.items.push(textItem);
 
-    const heightDownButton = new TextbuttonItem('height_down', '<', {
+    const heightDownButton = new ArrowbuttonItem('height_down', {
       x: 100,
       y: 182,
-    }, 50, 60, 28, '#555555', '#ffc338', '#929292', {
+    }, 50, 60, '#ffc338', '#00e5ff', 'down', {
       onTriggerPressed: () => {
         userPosition.y -= 0.05;
         textItem.text = userPosition.y.toFixed(2);
@@ -22,10 +23,10 @@ export default class CameraMenu extends BaseMenu {
       },
     });
 
-    const heightUpButton = new TextbuttonItem('height_up', '>', {
+    const heightUpButton = new ArrowbuttonItem('height_up', {
       x: 366,
       y: 182,
-    }, 50, 60, 28, '#555555', '#ffc338', '#929292', {
+    }, 50, 60, '#ffc338', '#00e5ff', 'up', {
       onTriggerPressed: () => {
         userPosition.y += 0.05;
         textItem.text = userPosition.y.toFixed(2);
