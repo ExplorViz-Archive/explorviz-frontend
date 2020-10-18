be4cee45ee56d7958395289081659d297d84116d
Malte Hansen <stu126317@mail.uni-kiel.de>
2020-10-18 02:07:06 +0200

    Add first version of user list menu (VR)

---
+++ tests/unit/utils/menus/user-list-menu-test.ts
@@ -0,0 +1,10 @@
+import UserListMenu from 'explorviz-frontend/utils/menus/user-list-menu';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | user-list-menu', function(/* hooks */) {
+
+  test('it exists', function(assert) {
+    // let result = UserListMenu();
+    assert.ok(true);
+  });
+});
