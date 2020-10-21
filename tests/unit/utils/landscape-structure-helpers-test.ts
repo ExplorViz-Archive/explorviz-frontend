28f9da13db6f14492383263b616a8f7aa0d79ddf
dan0x09 <danielkoenig95@web.de>
2020-10-21 22:30:41 +0200

    Remove tests for landscape structure helper

--- tests/unit/utils/landscape-structure-helpers-test.ts
+++ tests/unit/utils/landscape-structure-helpers-test.ts
@@ -1,11 +1,11 @@
-import landscapeStructureHelpers from 'explorviz-frontend/utils/landscape-structure-helpers';
-import { module, test } from 'qunit';
+/* import landscapeStructureHelpers from 'explorviz-frontend/utils/landscape-structure-helpers'; */
+import { module, /* test */ } from 'qunit';
 
-module('Unit | Utility | landscape-structure-helpers', function(hooks) {
+module('Unit | Utility | landscape-structure-helpers', function() {
 
   // Replace this with your real tests.
-  test('it works', function(assert) {
+/*   test('it works', function(assert) {
     let result = landscapeStructureHelpers();
     assert.ok(result);
-  });
+  }); */
 });
