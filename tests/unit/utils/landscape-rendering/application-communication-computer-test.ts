8298152b0854ca0eaa0a64ba1dc03b69054ecc98
dan0x09 <danielkoenig95@web.de>
2020-10-21 23:05:59 +0200

    Fix tests or remove test files, since they are not needed

--- tests/unit/utils/landscape-rendering/application-communication-computer-test.ts
+++ tests/unit/utils/landscape-rendering/application-communication-computer-test.ts
@@ -1,11 +1,11 @@
-import landscapeRenderingApplicationCommunicationComputer from 'explorviz-frontend/utils/landscape-rendering/application-communication-computer';
-import { module, test } from 'qunit';
+/* import landscapeRenderingApplicationCommunicationComputer from 'explorviz-frontend/utils/landscape-rendering/application-communication-computer'; */
+import { module, /* test */ } from 'qunit';
 
 module('Unit | Utility | landscape-rendering/application-communication-computer', function(hooks) {
 
   // Replace this with your real tests.
-  test('it works', function(assert) {
+/*   test('it works', function(assert) {
     let result = landscapeRenderingApplicationCommunicationComputer();
     assert.ok(result);
-  });
+  }); */
 });
--- tests/unit/utils/landscape-rendering/class-communication-computer-test.ts
+++ tests/unit/utils/landscape-rendering/class-communication-computer-test.ts
@@ -1,11 +1,11 @@
-import landscapeRenderingClassCommunicationComputer from 'explorviz-frontend/utils/landscape-rendering/class-communication-computer';
-import { module, test } from 'qunit';
+/* import landscapeRenderingClassCommunicationComputer from 'explorviz-frontend/utils/landscape-rendering/class-communication-computer'; */
+import { module, /* test */ } from 'qunit';
 
 module('Unit | Utility | landscape-rendering/class-communication-computer', function(hooks) {
 
   // Replace this with your real tests.
-  test('it works', function(assert) {
+/*   test('it works', function(assert) {
     let result = landscapeRenderingClassCommunicationComputer();
     assert.ok(result);
-  });
+  }); */
 });
