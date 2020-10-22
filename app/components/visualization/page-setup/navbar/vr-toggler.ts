49e7de0dadff659bf22d83265046b002cef14a70
Malte Hansen <stu126317@mail.uni-kiel.de>
2020-10-22 03:46:35 +0200

    Stop landscape listener when switching to VR mode

--- app/components/visualization/page-setup/navbar/vr-toggler.ts
+++ app/components/visualization/page-setup/navbar/vr-toggler.ts
@@ -1,13 +1,21 @@
 import { action } from '@ember/object';
 import Component from '@glimmer/component';
+import LandscapeListener from 'explorviz-frontend/services/landscape-listener';
+import { inject as service } from '@ember/service';
 
 interface VrTogglerArgs {
   switchToVR: () => void;
 }
 
 export default class VrToggler extends Component<VrTogglerArgs> {
+  @service('landscape-listener')
+  landscapeListener!: LandscapeListener;
+
   @action
   openVrMode() {
-    this.args.switchToVR();
+    if (this.landscapeListener.landscapeRepo.latestLandscape) {
+      this.landscapeListener.stopVisualizationReload();
+      this.args.switchToVR();
+    }
   }
 }
