8a6e9269dedb43732c105c9e7c3e0f98233979a9
Justin Andresen <jan.dresen.95@gmail.com>
2021-03-23 11:48:14 +0100

    Remove unused tests

--- tests/unit/utils/vr-helpers/multi-user-helper-test.ts
+++
@@ -1,8 +0,0 @@
-import { module, test } from 'qunit';
-
-module('Unit | Utility | vr-helpers/multi-user-helper', function( /* hooks */) {
-
-  test('dummy test', function(assert) {
-    assert.ok(true);
-  });
-});
