654d6658ea1b06bdaa22a2a952ebb1c27c4778c6
Justin Andresen <jan.dresen.95@gmail.com>
2021-03-25 16:30:00 +0100

    Remove multi-user configuration file
    The vr service address is now configured in `backend-addresses.js`

--- lib/virtual-reality/addon/services/web-socket.ts
+++ lib/virtual-reality/addon/services/web-socket.ts
@@ -1,58 +1,29 @@
 import Service, { inject as service } from '@ember/service';
+import ENV from 'explorviz-frontend/config/environment';
 import debugLogger from 'ember-debug-logger';
-import { AjaxServiceClass } from 'ember-ajax/services/ajax';
 
-type WebSocketConfiguration = {
-  host: string,
-  port: number,
-  secure: boolean,
-  path: string,
-};
-
-function isWebSocketConfiguration(config: any): config is WebSocketConfiguration {
-  return config !== null
-      && typeof config === 'object'
-      && typeof config.host === 'string'
-      && typeof config.port === 'number'
-      && typeof config.secure === 'boolean'
-      && typeof config.path === 'string'
-}
+const { vrService } = ENV.backendAddresses;
 
 export default class WebSocketService extends Service {
   @service()
   private websockets !: any;
 
-  @service('ajax')
-  private ajax!: AjaxServiceClass;
-
   private debug = debugLogger('WebSocketService');
 
-  private config: Promise<WebSocketConfiguration> | null = null;
   private currentSocket: any = null; // WebSocket to send/receive messages to/from backend
   private currentSocketUrl: string | null = null;
 
   socketCloseCallback: ((event: any) => void) | null = null;
   messageCallback: ((message: any) => void) | null = null;
 
-  private async loadConfiguration(file: string): Promise<WebSocketConfiguration> {
-    const config = await this.ajax.request(file);
-    if (isWebSocketConfiguration(config)) return config;
-    throw `invalid web socket configuration ${file}`;
-  }
-
-  private getSocketUrl(config: WebSocketConfiguration, roomId: string) {
-    // Insert room id into the configured path and make sure that there are
-    // no leading or trailing slashes.
-    const path = config.path.replace('{room-id}', encodeURIComponent(roomId)).replace(/^\/|\/$/g, "");
-    return `${config.secure ? "wss" : "ws"}://${config.host}:${config.port}/${path}`;
+  private getSocketUrl(roomId: string) {
+    const vrServiceSocket = vrService.replace(/^http(s?):\/\//i, 'ws$1://');
+    return `${vrServiceSocket}/v2/vr/${roomId}`;
   }
 
   async initSocket(roomId: string) {
-    // Load configuration file at most once.
-    this.config = this.config || this.loadConfiguration('config/config_multiuser.json');
-    const config = await this.config;
-
-    this.currentSocketUrl = this.getSocketUrl(config, roomId);
+    // TODO set Authorization header
+    this.currentSocketUrl = this.getSocketUrl(roomId);
     this.currentSocket = this.websockets.socketFor(this.currentSocketUrl);
     this.currentSocket.on('message', this.messageHandler, this);
     this.currentSocket.on('close', this.closeHandler, this);
