69b94740e968218ce51b98e6e4d9558666c96892
dan0x09 <stu126132@mail.uni-kiel.de>
2021-04-12 19:17:13 +0200

    Fix styling of class files and remove unused getters and class fields

--- lib/collaborative-mode/addon/utils/collaborative-data.ts
+++ lib/collaborative-mode/addon/utils/collaborative-data.ts
@@ -8,40 +8,40 @@ export enum CollaborativeEvents {
   RequestLastPosition = 'request_last_position',
   TransferControl = 'transfer_control',
   OpenLandscapeView = 'open_landscape_view',
-  CreateMeeting = "create_meeting",
-  JoinMeeting = "join_meeting",
-  LeaveMeeting = "leave_meeting",
-  ApplicationOpened = "application_opened",
-  TogglePresentationMode = "toggle_presentation_mode"
+  CreateMeeting = 'create_meeting',
+  JoinMeeting = 'join_meeting',
+  LeaveMeeting = 'leave_meeting',
+  ApplicationOpened = 'application_opened',
+  TogglePresentationMode = 'toggle_presentation_mode',
 }
 
 export interface Perspective {
   user?: string;
-  position: number[],
-  rotation: number[],
-  requested: boolean
+  position: number[];
+  rotation: number[];
+  requested: boolean;
 }
 
 export interface CursorPosition {
-  user?: string,
-  point: number[],
-  id?: string
+  user?: string;
+  point: number[];
+  id?: string;
 }
 
 export interface Click {
-  user: string,
-  id: string
+  user: string;
+  id: string;
 }
 export interface IdentifiableMesh {
-  colabId: string
+  colabId: string;
 }
 
 export interface UserJoinedMessage {
-  user: string
+  user: string;
 }
 
 export interface PresentationModeActivated {
-  user: string
+  user: string;
 }
 
 export interface PresentationModeDeactivated {
@@ -52,18 +52,18 @@ export function instanceOfIdentifiableMesh(object: any): object is IdentifiableM
 }
 
 export interface Meeting {
-  id: string,
-  landscapeToken: string,
-  users: User[],
-  presentationMode: boolean,
-  adminId: string,
-  presenterId: string,
-  applicationId: string
+  id: string;
+  landscapeToken: string;
+  users: User[];
+  presentationMode: boolean;
+  adminId: string;
+  presenterId: string;
+  applicationId: string;
 }
 
 export interface User {
-  name: string,
-  color: string,
-  admin: boolean,
-  presenter: boolean
+  name: string;
+  color: string;
+  admin: boolean;
+  presenter: boolean;
 }
