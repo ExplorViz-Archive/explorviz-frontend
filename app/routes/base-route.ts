fff324feaece7f5d8c2cd6454185f18cd5fe1ea1
dan0x09 <stu126132@mail.uni-kiel.de>
2021-04-20 17:27:00 +0200

    Add error handling for auth0 responses

--- app/routes/base-route.ts
+++ app/routes/base-route.ts
@@ -1,6 +1,8 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import { Auth0Error } from 'auth0-js';
 import Auth from 'explorviz-frontend/services/auth';
+import AlertifyHandler from 'explorviz-frontend/utils/alertify-handler';
 
 export default class BaseRoute extends Route {
   @service
@@ -9,6 +11,13 @@ export default class BaseRoute extends Route {
   async beforeModel() {
     // this is where we check if a user is authenticated
     // if not authenticated, kick them to the home page
-    await this.auth.checkLogin().catch(() => this.transitionTo('/'));
+    await this.auth.checkLogin().catch((e: Auth0Error) => {
+      if (e.description) {
+        AlertifyHandler.showAlertifyWarning(e.description);
+      }
+      if (e.statusCode !== 429) {
+        this.auth.logout();
+      }
+    });
   }
 }
