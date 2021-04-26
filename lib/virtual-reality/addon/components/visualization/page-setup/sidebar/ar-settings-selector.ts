807c2f268659568e0c57c967b9734aa06b6c2d89
Malte Hansen <malte.hanseat@gmail.com>
2021-04-26 19:49:59 +0200

    Add sidebar settings for configure the UI for AR

--- lib/virtual-reality/addon/components/visualization/page-setup/sidebar/ar-settings-selector.ts
+++ lib/virtual-reality/addon/components/visualization/page-setup/sidebar/ar-settings-selector.ts
@@ -2,6 +2,7 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import ArSettings from 'virtual-reality/services/ar-settings';
 import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 
 interface ArSettingsSelectorArgs {
   removeComponent(componentPath: string): void
@@ -10,6 +11,12 @@ interface ArSettingsSelectorArgs {
 export default class ArSettingsSelector extends Component<ArSettingsSelectorArgs> {
   @service('ar-settings')
   arSettings!: ArSettings;
+
+  @tracked
+  buttonSize = 5;
+
+  @tracked
+  buttonPadding = 2;
 
   @action
   close() {
@@ -24,5 +31,28 @@ export default class ArSettingsSelector extends Component<ArSettingsSelectorArgs
   @action
   updateApplicationOpacity(event: any) {
     this.arSettings.setApplicationOpacity(event.target.value);
+  }
+
+  @action
+  updateButtonSize(event: any) {
+    const size = event.target.value;
+    this.buttonSize = size;
+
+    ArSettingsSelector.setCssVariable('--ar-button-size', `${size}vmin`);
+  }
+
+  @action
+  updateButtonSpacing(event: any) {
+    const padding = event.target.value;
+    this.buttonPadding = padding;
+
+    ArSettingsSelector.setCssVariable('--ar-button-padding', `${padding}vmin`);
+  }
+
+  static setCssVariable(variable: string, value: string) {
+    const root = document.querySelector(':root');
+    if (root) {
+      (<HTMLElement>root).style.setProperty(variable, value);
+    }
   }
 }
