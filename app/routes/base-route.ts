b67afd28edb4ba5b6fb5ec1a204b195acb564f2e
dan0x09 <stu126132@mail.uni-kiel.de>
2021-04-28 13:49:39 +0200

    Improve error handling regarding auth0 and font loading

--- app/routes/base-route.ts
+++ app/routes/base-route.ts
@@ -1,5 +1,6 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
 import { Auth0Error } from 'auth0-js';
 import Auth from 'explorviz-frontend/services/auth';
 import AlertifyHandler from 'explorviz-frontend/utils/alertify-handler';
@@ -11,18 +12,17 @@ export default class BaseRoute extends Route {
   async beforeModel() {
     // this is where we check if a user is authenticated
     // if not authenticated, kick them to the home page
-    return new Promise<void>((resolve, reject) => {
-      this.auth.checkLogin().then(() => {
-        resolve();
-      }).catch((e: Auth0Error) => {
-        if (e.description) {
-          AlertifyHandler.showAlertifyWarning(e.description);
-        }
-        if (e.statusCode !== 429) {
-          this.auth.logout();
-        }
-        reject();
-      });
-    });
+    return this.auth.checkLogin();
+  }
+
+  @action
+  error(error: Auth0Error) {
+    if (error.description) {
+      AlertifyHandler.showAlertifyWarning(error.description);
+    }
+    if (error.statusCode !== 429) {
+      this.auth.logout();
+    }
+    return true;
   }
 }
--- app/routes/visualization.ts
+++ app/routes/visualization.ts
@@ -3,6 +3,8 @@ import THREE from 'three';
 import debugLogger from 'ember-debug-logger';
 import LandscapeTokenService from 'explorviz-frontend/services/landscape-token';
 import { inject as service } from '@ember/service';
+import AlertifyHandler from 'explorviz-frontend/utils/alertify-handler';
+import { action } from '@ember/object';
 import BaseRoute from './base-route';
 
 /**
@@ -20,11 +22,19 @@ export default class VisualizationRoute extends BaseRoute {
   async beforeModel() {
     if (this.landscapeToken.token === null) {
       this.transitionTo('landscapes');
+      return Promise.resolve();
     }
+    // load font for labels
+    const controller = this.controllerFor('visualization') as VisualizationController;
+    if (!controller.font) {
+      const font = await this.loadFont();
+      controller.set('font', font);
+    }
+    // handle auth0 authorization
     return super.beforeModel();
   }
 
-  model() {
+  private async loadFont(): Promise<THREE.Font> {
     return new Promise((resolve, reject) => {
       new THREE.FontLoader().load(
         // resource URL
@@ -42,6 +52,15 @@ export default class VisualizationRoute extends BaseRoute {
         },
       );
     });
+  }
+
+  @action
+  error(error: any) {
+    if (error instanceof ProgressEvent) {
+      AlertifyHandler.showAlertifyError('Failed to load font for labels.');
+      return true;
+    }
+    return super.error(error);
   }
 
   // @Override
