cc9b7906799761f9d2643b4f638075b9cf11e1e1
Malte Hansen <malte.hanseat@gmail.com>
2021-05-02 20:48:43 +0200

    Fix linting errors

--- tests/unit/services/ar-settings-test.ts
+++ tests/unit/services/ar-settings-test.ts
@@ -1,13 +1,12 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
-module('Unit | Service | ar-settings', function(hooks) {
+module('Unit | Service | ar-settings', function (hooks) {
   setupTest(hooks);
 
   // Replace this with your real tests.
-  test('it exists', function(assert) {
-    let service = this.owner.lookup('service:ar-settings');
+  test('it exists', function (assert) {
+    const service = this.owner.lookup('service:ar-settings');
     assert.ok(service);
   });
 });
-
