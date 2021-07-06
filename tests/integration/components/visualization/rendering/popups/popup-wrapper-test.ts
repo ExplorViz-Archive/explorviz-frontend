6580ecdc40d09ac2119eda1942eb10c3c26bdc00
Malte Hansen <malte.hanseat@gmail.com>
2021-07-06 23:37:43 +0200

    Add folders for AR popups and sidebar resizer

--- tests/integration/components/visualization/rendering/popups/popup-wrapper-test.ts
+++ tests/integration/components/visualization/rendering/popups/popup-wrapper-test.ts
@@ -3,15 +3,14 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | ar-popup-coordinator', function (hooks) {
+module('Integration | Component | visualization/rendering/popups/popup-wrapper', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`<ArPopupCoordinator />`);
-
+    await render(hbs`<Visualization::Rendering::Popups::PopupWrapper />`);
     assert.equal(this.element.textContent?.trim(), '');
   });
 });
