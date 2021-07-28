fd6ea05b0be1d27b7d57167ee43c053fa3c19d72
dan0x09 <stu126132@mail.uni-kiel.de>
2021-07-28 11:13:38 +0200

    Add sidebar component for extended information on communication

---
+++ app/components/visualization/page-setup/sidebar/extended-communication-information.ts
@@ -0,0 +1,10 @@
+import Component from '@glimmer/component';
+import { DrawableClassCommunication } from 'explorviz-frontend/utils/landscape-rendering/class-communication-computer';
+
+interface Args {
+  communication: DrawableClassCommunication;
+}
+
+export default class ExtendedCommunicationInformation extends Component<Args> {
+
+}
