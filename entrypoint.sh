c1ddb00e3bb2d1ecf0a5008c711b1be432a67bf0
Alexander Krause <akr@informatik.uni-kiel.de>
2021-12-16 14:21:30 +0100

    prod support for auth0

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,8 @@ sed -i "s#change-trace-url#$TRACE_URL#g" /usr/share/nginx/html/index.html
 sed -i "s#change-user-url#$USER_URL#g" /usr/share/nginx/html/index.html
 sed -i "s#change-collab-url#$COLLAB_URL#g" /usr/share/nginx/html/index.html
 sed -i "s#change-frontend-host-name#$FRONTEND_HOST_NAME#g" /usr/share/nginx/html/index.html
+sed -i "s#change-auth0-logo-url#$AUTH0_LOGO_URL#g" /usr/share/nginx/html/index.html
+sed -i "s#change-auth0-callback-url#$AUTH0_CALLBACK_URL#g" /usr/share/nginx/html/index.html
+sed -i "s#change-auth0-logout-url#$AUTH0_LOGOUT_URL#g" /usr/share/nginx/html/index.html
 
 exec "$@"
