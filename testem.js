d96b7d8530e59004733931f91f73630f60bdf277
Alexander Krause <akr@informatik.uni-kiel.de>
2022-03-07 16:38:57 +0100

    reduce testem config

--- testem.js
+++ testem.js
@@ -3,25 +3,13 @@
 module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
-  launch_in_ci: ['chrome'],
-  launch_in_dev: ['chrome'],
+  launch_in_ci: ['Chrome'],
+  launch_in_dev: ['Chrome'],
   browser_start_timeout: 120,
   browser_args: {
-    chrome: {
+    Chrome: {
       ci: [
-        // --no-sandbox is needed when running chrome inside a container
-        process.env.CI ? '--no-sandbox' : null,
-        '--headless',
-        '--disable-dev-shm-usage',
-        '--disable-software-rasterizer',
-        '--mute-audio',
-        '--remote-debugging-port=0',
-        '--window-size=1440,900',
-      ].filter(Boolean),
-    },
-    chromium: {
-      ci: [
-        // --no-sandbox is needed when running chromium inside a container
+        // --no-sandbox is needed when running Chrome inside a container
         process.env.CI ? '--no-sandbox' : null,
         '--headless',
         '--disable-dev-shm-usage',
